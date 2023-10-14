cce74b99eb53c8b8827310f7bc407b96e114a06d
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-15 02:27:39 +0900

    feat #9 : Map 생성

---
+++ Outline/Outline/Source/View/RecomendedCourses/RecomendedCourseMap.swift
@@ -0,0 +1,55 @@
+//
+//  Map.swift
+//  Outline
+//
+//  Created by Hyunjun Kim on 10/15/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct RecomendedCourseMap: UIViewRepresentable {
+    
+    var camera: MKMapCamera
+    var coordinates: [CLLocationCoordinate2D] = []
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.showsUserLocation = true
+
+        mapView.camera = camera
+        mapView.isUserInteractionEnabled = false
+        
+        let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        mapView.addOverlay(polyline)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: RecomendedCourseMap
+        
+        init(_ parent: RecomendedCourseMap) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let coursePolyline = overlay as? MKPolyline {
+                let renderer = MKPolylineRenderer(polyline: coursePolyline)
+                renderer.strokeColor = UIColor.orange
+                renderer.lineWidth = 3
+                renderer.alpha = 0.5
+                return renderer
+            }
+            return MKOverlayRenderer()
+        }
+    }
+}
