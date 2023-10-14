83b03a479b6180040c5b5ad0463f7ba71016f135
Moonkey <moonkey48@icloud.com>
2023-10-14 10:44:25 +0900

    test #1 : FireStore 연결 확인을 위한 테스트 FireStoreManager 구현

--- Outline/Outline/Source/Model/FireStore/FireStoreManager.swift
+++ Outline/Outline/Source/Model/FireStore/FireStoreManager.swift
@@ -5,8 +5,34 @@
 //  Created by Seungui Moon on 10/14/23.
 //
 
+import Firebase
+import FirebaseFirestoreSwift
 import SwiftUI
 
 class FireStoreManager: ObservableObject {
+    @Published var nickName: String = ""
+    @Published var height: Int = 0
+    let db = Firestore.firestore()
     
+    init() {
+       fetchData()
+    }
+
+    func fetchData() {
+        let docRef = db.collection("users").document("4JACo00t3iuK8dfyDiFx")
+        docRef.getDocument { (document, error) in
+            guard error == nil else {
+                print("error", error ?? "")
+                return
+            }
+
+            if let document = document, document.exists {
+                let data = document.data()
+                if let data = data {
+                    self.nickName = data["nickname"] as? String ?? ""
+                    self.height = data["height"] as? Int ?? 0
+                }
+            }
+        }
+    }
 }
