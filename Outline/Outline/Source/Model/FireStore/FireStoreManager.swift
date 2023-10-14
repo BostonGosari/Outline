1afdaa32e80968c1301833963fcf0996ba6dd099
Moonkey <moonkey48@icloud.com>
2023-10-14 10:13:24 +0900

    feat #1 : FirebaseApp 초기 세팅 및 environmentObject 추가

---
+++ Outline/Outline/Source/Model/FireStore/FireStoreManager.swift
@@ -0,0 +1,12 @@
+//
+//  FireStoreManager.swift
+//  Outline
+//
+//  Created by Seungui Moon on 10/14/23.
+//
+
+import SwiftUI
+
+class FireStoreManager: ObservableObject {
+    
+}
--- Outline/Outline/Source/OutlineApp.swift
+++ Outline/Outline/Source/OutlineApp.swift
@@ -5,13 +5,21 @@
 //  Created by Hyunjun Kim on 10/13/23.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct OutlineApp: App {
+    @StateObject private var fireStoreManager = FireStoreManager()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(fireStoreManager)
         }
     }
 }
