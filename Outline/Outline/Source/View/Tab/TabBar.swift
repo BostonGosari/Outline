95996ec7d32ffb3dcb3f1526b00f04c39021cd6e
hyunjun <hyunjuntyler@gmail.com>
2023-10-14 21:33:03 +0900

    feat #5 : 탭바 생성

---
+++ Outline/Outline/Source/View/Tab/TabBar.swift
@@ -0,0 +1,57 @@
+//
+//  TabBar.swift
+//  Outline
+//
+//  Created by Hyunjun Kim on 10/14/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    
+    @Binding var selectedTab: Tab
+        
+    private let tabIconSize: CGFloat = 18
+    private let tabTextSize: CGFloat = 10
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(tabItems) { item in
+                    Button {
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 5) {
+                            Image(systemName: item.icon)
+                                .font(.system(size: tabIconSize))
+                            Text(item.text)
+                                .font(.system(size: tabTextSize))
+                        }
+                        .foregroundStyle(selectedTab == item.tab ? .purple : .gray)
+                    }
+                    .buttonStyle(TabButtonStyle())
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(.top, 11)
+            .padding(.bottom, 18)
+            .background(
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .foregroundStyle(.thinMaterial)
+                    .shadow(color: .white, radius: 0.6, y: -0.5)
+            )
+        }
+    }
+}
+
+struct TabButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.97 : 1)
+            .animation(.easeInOut, value: configuration.isPressed)
+    }
+}
+
+#Preview {
+    TabBar(selectedTab: .constant(Tab.GPSArtRunning))
+}
