ec18a345e999836d5c979bd7e48ac0e79f29ffa4
hyunjun <hyunjuntyler@gmail.com>
2023-10-15 16:39:43 +0900

    chore #16 : ViewExtension 추가

--- Outline/Outline/Source/DesignSystem/Extension/ViewExtension.swift
+++ Outline/Outline/Source/DesignSystem/Extension/ViewExtension.swift
@@ -5,4 +5,15 @@
 //  Created by Hyunjun Kim on 10/13/23.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+        
+    /// 원하는 코너에 라운딩을 적용하는  `View Extension`
+    /// - Parameters:
+    ///   - radius: 라운딩의 정도
+    ///   - corners: 원하는 코너 [.topLeft, .topRight] 의 **배열 형식**으로 입력해주세요
+    func roundedCorners(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCornersShape(radius: radius, corners: corners))
+    }
+}
