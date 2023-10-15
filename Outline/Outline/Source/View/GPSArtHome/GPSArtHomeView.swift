c79f578a4e93fbaf527167cd1b4a911523965172
chamroro <chamsae@handong.ac.kr>
2023-10-15 19:37:24 +0900

    design : GPS Art Home View

---
+++ Outline/Outline/Source/View/GPSArtHome/GPSArtHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  GPSArtHomeView.swift
+//  Outline
+//
+//  Created by 김하은 on 10/15/23.
+//
+
+import SwiftUI
+
+struct GPSArtHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GPSArtHomeView()
+}
