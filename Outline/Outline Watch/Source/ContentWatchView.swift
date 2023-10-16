8fd08f6e1ea941e800ee3840c4c9ccafc8aebf98
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-16 20:30:08 +0900

    rename #20 : 워치 파일 폴더 수정

---
+++ Outline/Outline Watch/Source/ContentWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentWatchView.swift
+//  Outline Watch App
+//
+//  Created by Hyunjun Kim on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentWatchView: View {
+    var body: some View {
+        WatchTabView()
+    }
+}
+
+#Preview {
+    ContentWatchView()
+}
