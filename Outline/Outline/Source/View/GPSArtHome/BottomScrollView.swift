213a8d0ad896458eea95fe54ee0ba42b31273cce
chamroro <chamsae@handong.ac.kr>
2023-10-16 22:04:27 +0900

    design : 하단 Carousel View

---
+++ Outline/Outline/Source/View/GPSArtHome/BottomScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomScrollView.swift
+//  Outline
+//
+//  Created by 김하은 on 10/16/23.
+//
+
+import SwiftUI
+
+struct BottomScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomScrollView()
+}
