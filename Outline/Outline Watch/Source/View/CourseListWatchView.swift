63cf4185171f7944b2d67e5ec886103444a78efe
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-17 01:34:41 +0900

    design #20 : 리스트뷰 watchOS 10 스크롤 적용

--- Outline/Outline Watch/Source/View/CourseListWatchView.swift
+++ Outline/Outline Watch/Source/View/CourseListWatchView.swift
@@ -10,54 +10,70 @@ import SwiftUI
 struct CourseListWatchView: View {
     var body: some View {
         NavigationStack {
-            List {
-                Button {
-                    // action here
-                } label: {
-                    HStack {
-                        Image(systemName: "play.circle")
-                        Text("자유코스")
-                    }
-                    .foregroundColor(.black)
-                    .frame(maxWidth: .infinity)
-                }
-                .listRowBackground(
-                    Color.green
-                        .clipped()
-                        .cornerRadius(12)
-                )
-                
-                ForEach(0..<5) { _ in
+            ScrollView {
+                VStack(spacing: -5) {
                     Button {
-                        print("button clicked")
+                        // action here
                     } label: {
-                        VStack {
-                            Text("시티런")
-                                .padding(.leading, 4)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .overlay(alignment: .trailing) {
-                                    Button {
-                                        print("ellipsis clicked")
-                                    } label: {
-                                        Image(systemName: "ellipsis")
-                                            .font(.system(size: 24))
-                                            .frame(width: 48, height: 48)
-                                    }
-                                    .buttonStyle(.plain)
-                                    .padding(.trailing, -4)
-                                }
-                            Circle()
-                                .stroke(lineWidth: 4)
-                                .foregroundColor(.green)
+                        HStack {
+                            Image(systemName: "play.circle")
+                            Text("자유코스")
                         }
-                        .padding(.vertical, 16)
-                        .frame(height: 136)
+                        .foregroundColor(.black)
+                        .frame(height: 48)
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                                .foregroundStyle(.green)
+                        }
                     }
-                    .listRowBackground(
-                        Color.secondary
-                            .clipped()
-                            .cornerRadius(24)
-                    )
+                    .buttonStyle(.plain)
+                    .scrollTransition { content, phase in
+                        content
+                            .scaleEffect(phase.isIdentity ? 1 : 0.8)
+                            .opacity(phase.isIdentity ? 1 : 0.8)
+                    }
+                    .padding(.bottom, 8)
+                    
+                    ForEach(0..<5) { _ in
+                        Button {
+                            print("button clicked")
+                        } label: {
+                            VStack {
+                                Text("시티런")
+                                    .padding(.leading, 4)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .overlay(alignment: .trailing) {
+                                        Button {
+                                            print("ellipsis clicked")
+                                        } label: {
+                                            Image(systemName: "ellipsis")
+                                                .font(.system(size: 24))
+                                                .frame(width: 48, height: 48)
+                                        }
+                                        .buttonStyle(.plain)
+                                        .padding(.trailing, -4)
+                                    }
+                                Circle()
+                                    .stroke(lineWidth: 4)
+                                    .foregroundColor(.green)
+                            }
+                            .padding(.vertical, 16)
+                            .padding(.horizontal, 8)
+                            .frame(height: 136)
+                            .frame(maxWidth: .infinity)
+                            .background {
+                                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                        .buttonStyle(.plain)
+                        .scrollTransition { content, phase in
+                            content
+                                .scaleEffect(phase.isIdentity ? 1 : 0.8)
+                                .opacity(phase.isIdentity ? 1 : 0.8)
+                        }
+                    }
                 }
             }
             .navigationTitle("러닝")
