77eb0a7986145875609b11dd20333f7f4b8e372b
chamroro <chamsae@handong.ac.kr>
2023-10-18 22:18:11 +0900

    feat : watchOS 워크아웃 러닝중 탭뷰 3개

--- Outline/Outline Watch/Source/View/ControlsView.swift
+++ Outline/Outline Watch/Source/View/ControlsView.swift
@@ -9,31 +9,97 @@ import SwiftUI
 
 struct ControlsView: View {
     @EnvironmentObject var workoutManager: WatchWorkoutManager
-
     var body: some View {
-        HStack {
-            VStack {
+        VStack {
+            Text(workoutManager.running ? "시티런" : "일시정지됨")
+               .bold()
+               .foregroundStyle(.green)
+               .padding()
+               .padding(.top, 5)
+               .padding(.leading, 20)
+               .frame(maxWidth: .infinity, alignment: .topLeading)
+               .background {
+                   Rectangle()
+                       .foregroundStyle(.thinMaterial)
+               }
+            Spacer()
+            HStack(spacing: 14) {
                 Button {
                     workoutManager.endWorkout()
                 } label: {
-                    Image(systemName: "xmark")
+                    Image(systemName: "stop.fill")
+                        .font(.system(size: 24))
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                        .padding()
                 }
-                .tint(.red)
-                .font(.title2)
-                Text("End")
-            }
-            VStack {
-                Button {
-                    workoutManager.togglePause()
-                } label: {
-                    Image(systemName: workoutManager.running ? "pause" : "play")
+                .overlay(
+                    Circle()
+                        .stroke(Color.white, lineWidth: 2)
+                        .frame(width: 80, height: 80)
+                        .background(
+                            Circle()
+                                .foregroundColor(.white).opacity(0.5)
+                        )
+                )
+                .frame(width: 80, height: 80)
+                .buttonStyle(.plain)
+                if workoutManager.running {
+                    Button {
+                        workoutManager.togglePause()
+                    } label: {
+                        Image(systemName: "pause")
+                            .font(.system(size: 24))
+                            .fontWeight(.black)
+                            .foregroundColor(.first)
+                            .padding()
+                    }
+                    .overlay(
+                        Circle()
+                            .stroke(Color.first, lineWidth: 2)
+                            .frame(width: 80, height: 80)
+                            .background(
+                                Circle()
+                                    .foregroundColor(.first).opacity(0.5)
+                            )
+                        
+                    )
+                    .frame(width: 80, height: 80)
+                    .buttonStyle(.plain)
+                    
+                } else {
+                   
+                    Button {
+                        workoutManager.togglePause()
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .font(.system(size: 24))
+                            .fontWeight(.black)
+                            .foregroundColor(.first)
+                            .padding()
+                    }
+                    .overlay(
+                        Circle()
+                            .stroke(Color.first, lineWidth: 2)
+                            .frame(width: 80, height: 80)
+                            .background(
+                                Circle()
+                                    .foregroundColor(.first).opacity(0.5)
+                            )
+                    )
+                    .frame(width: 80, height: 80)
+                    .buttonStyle(.plain)
+                    
                 }
-                .tint(.yellow)
-                .font(.title2)
-                Text(workoutManager.running ? "Pause" : "Resume")
             }
+            .frame(width: .infinity, height: .infinity)
+            .padding(.bottom, 20)
+            Spacer()
         }
+        .ignoresSafeArea()
+       
     }
+    
 }
 
 #Preview {
