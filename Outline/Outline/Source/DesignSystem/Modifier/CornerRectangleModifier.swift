1ebd8d11c714a545b4f70d5001a55e9f8251e197
Hyebin (Helia) <hyebin218@naver.com>
2023-10-20 01:59:55 +0900

    add #46 : asset 추가 및 디자인 시스템

---
+++ Outline/Outline/Source/DesignSystem/Modifier/CornerRectangleModifier.swift
@@ -0,0 +1,24 @@
+//
+//  CornerRectangleModifier.swift
+//  Outline
+//
+//  Created by hyebin on 10/20/23.
+//
+
+import SwiftUI
+
+struct CornerRectangleModifier: ViewModifier {
+    let topLeft: CGFloat
+    let topRight: CGFloat
+    let bottom: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+              CardBorder(cornerRadiusTopLeft: topLeft, cornerRadiusTopRight: topRight, cornerRadiusBottomLeft: bottom, cornerRadiusBottomRight: bottom)
+            )
+            .roundedCorners(topLeft, corners: [.topLeft])
+            .roundedCorners(topRight, corners: [.topRight])
+            .roundedCorners(bottom, corners: [.bottomLeft, .bottomRight])
+    }
+}
