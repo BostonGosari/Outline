182965c06a22f4bc2208aa5f96e4c921c03a0207
Moonkey <moonkey48@icloud.com>
2023-10-19 21:02:07 +0900

    feat #51 : FireStore에 course 추가 기능 구현

--- Outline/Outline/Source/View/DataTest/CourseDataUploadView.swift
+++ Outline/Outline/Source/View/DataTest/CourseDataUploadView.swift
@@ -15,24 +15,43 @@ struct CourseDataUploadView: View {
     var body: some View {
         VStack {
             Button {
-                let parsedCoordinates = parseCooridinates(fileName: "duckRun")
+                let parsedCoordinates = parseCooridinates(fileName: "pohangDangDangRun")
+                let centerLocation  = CLLocationCoordinate2D(latitude: 36.0259635, longitude: 129.361410)
+                getPlaceMarks(coordinate: centerLocation) { newPlaceMark in
+                    let gpsArtCourse = GPSArtCourse(
+                        id: UUID().uuidString,
+                        courseName: "댕댕런",
+                        locationInfo: newPlaceMark,
+                        courseLength: 5,
+                        courseDuration: 50,
+                        centerLocation: Coordinate(longitude: centerLocation.longitude, latitude: centerLocation.latitude),
+                        distance: 0,
+                        level: .hard,
+                        alley: .lots,
+                        coursePaths: parsedCoordinates,
+                        heading: 1.5,
+                        thumbnail: "https://firebasestorage.googleapis.com/v0/b/outline-5640c.appspot.com/o/courseImages%2FcourseImage_duckRun.jpg?alt=media&token=479a261d-0534-4e21-ac27-a9b2b27bfce7&_gl=1*4pvvwz*_ga*MTE2MDQ4NzcyNy4xNjk3MTE2NTg4*_ga_CW55HF8NVT*MTY5NzcxNDc1Ny4zMy4xLjE2OTc3MTUyOTEuNTEuMC4w"
+                    )
+                    coureDataUploadMananger.uploadData(course: gpsArtCourse)
+                }
             } label: {
-                Text("parseData")
+                Text("write data")
             }
         }
     }
     
-    func parseCooridinates(fileName: String) -> [CLLocationCoordinate2D] {
+    func parseCooridinates(fileName: String) -> [Coordinate] {
         if let kmlFilePath = Bundle.main.path(forResource: fileName, ofType: "kml") {
             let kmlParser = KMLParserManager()
-            return kmlParser.parseKMLFile(atPath: kmlFilePath)
+            return kmlParser.parseKMLFile(atPath: kmlFilePath).map { clLocation2D in
+                Coordinate(longitude: clLocation2D.longitude, latitude: clLocation2D.latitude)
+            }
         }
         return []
     }
     
-    func getPlaceMarks(coordinate: CLLocationCoordinate2D) -> Placemark? {
+    func getPlaceMarks(coordinate: CLLocationCoordinate2D, completion: @escaping (_ newPlaceMark: Placemark) -> Void) {
         let geocoder = CLGeocoder()
-        var placemardResult: Placemark?
         geocoder.reverseGeocodeLocation(CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) { placemarks, error in
             if let error = error {
                 print("Reverse geocoding error: \(error.localizedDescription)")
@@ -41,7 +60,7 @@ struct CourseDataUploadView: View {
                 let city = placemark.locality ?? ""
                 let town = placemark.subLocality ?? ""
                 
-                placemardResult = Placemark(
+                completion(Placemark(
                     name: placemark.name ?? "",
                     isoCountryCode: placemark.isoCountryCode ?? "",
                     administrativeArea: placemark.administrativeArea ?? "",
@@ -50,11 +69,10 @@ struct CourseDataUploadView: View {
                     subLocality: placemark.subLocality ?? "",
                     throughfare: placemark.thoroughfare ?? "",
                     subThroughfare: placemark.subThoroughfare ?? ""
-                )
+                ))
             }
         }
         
-        return placemardResult
     }
 }
 
