1ed334bdf1fe9ae5b0f777b87033818eaa657268
Moonkey <moonkey48@icloud.com>
2023-10-19 19:23:10 +0900

    feat #51 : 위도 경도에 따른 Placemark 기능 구현

--- Outline/Outline/Source/View/DataTest/CourseDataUploadView.swift
+++ Outline/Outline/Source/View/DataTest/CourseDataUploadView.swift
@@ -29,6 +29,33 @@ struct CourseDataUploadView: View {
         }
         return []
     }
+    
+    func getPlaceMarks(coordinate: CLLocationCoordinate2D) -> Placemark? {
+        let geocoder = CLGeocoder()
+        var placemardResult: Placemark?
+        geocoder.reverseGeocodeLocation(CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)) { placemarks, error in
+            if let error = error {
+                print("Reverse geocoding error: \(error.localizedDescription)")
+            } else if let placemark = placemarks?.first {
+                let area = placemark.administrativeArea ?? ""
+                let city = placemark.locality ?? ""
+                let town = placemark.subLocality ?? ""
+                
+                placemardResult = Placemark(
+                    name: placemark.name ?? "",
+                    isoCountryCode: placemark.isoCountryCode ?? "",
+                    administrativeArea: placemark.administrativeArea ?? "",
+                    subAdministrativeArea: placemark.subAdministrativeArea ?? "",
+                    locality: placemark.locality ?? "",
+                    subLocality: placemark.subLocality ?? "",
+                    throughfare: placemark.thoroughfare ?? "",
+                    subThroughfare: placemark.subThoroughfare ?? ""
+                )
+            }
+        }
+        
+        return placemardResult
+    }
 }
 
 #Preview {
