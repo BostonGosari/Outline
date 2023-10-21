b3e187a8bb1048afbe615b76dcc06cdf2f5c2c13
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-21 14:42:43 +0900

    feat #53 : 러닝 뷰모델 적용

--- Outline/Outline/Source/View/Running/WorkoutDataView.swift
+++ Outline/Outline/Source/View/Running/WorkoutDataView.swift
@@ -1,24 +1,31 @@
 import SwiftUI
 
 struct WorkoutDataView: View {
+    
+    @ObservedObject var runningViewModel: RunningViewModel
+    let weight: Double = 60
+
     var body: some View {
         ZStack {
             Color("Gray900").ignoresSafeArea()
             VStack {
-                strokeText(text: "00:20.53", width: 2, color: Color.primaryColor)
+                strokeText(text: "\(runningViewModel.distance)", width: 2, color: Color.primaryColor)
                     .font(
                         Font.custom("Pretendard-ExtraBold", size: 64)
                     )
                 Spacer()
                 LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 36) {
-                    workoutDataItem(value: "1.22/5", label: "킬로미터")
-                    workoutDataItem(value: "--", label: "평균 페이스")
-                    workoutDataItem(value: "232", label: "칼로리")
-                    workoutDataItem(value: "102", label: "BPM")
+                    workoutDataItem(value: "\(runningViewModel.distance)", label: "킬로미터")
+                    workoutDataItem(value: "\(runningViewModel.avgPace / 60 * 1000)", label: "평균 페이스")
+                    workoutDataItem(value: "\(runningViewModel.kilocalorie)", label: "칼로리")
+                    workoutDataItem(value: "--", label: "BPM")
                 }
             }
             .frame(height: 300)
             .padding()
+            .onChange(of: runningViewModel.distance) { _, distance in
+                runningViewModel.kilocalorie = weight * distance / 1000 * 1.036
+            }
         }
     }
 }
@@ -56,6 +63,6 @@ extension WorkoutDataView {
 
 struct WorkoutDataView_Previews: PreviewProvider {
     static var previews: some View {
-        WorkoutDataView()
+        WorkoutDataView(runningViewModel: RunningViewModel())
     }
 }
