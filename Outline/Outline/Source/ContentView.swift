6ecadc241f6ff81ca2a19af1330bd795fa6f838c
Moonkey <moonkey48@icloud.com>
2023-10-22 21:55:08 +0900

    feat #63 : ContentView에서 authState에 따른 분기처리 구현

--- Outline/Outline/Source/ContentView.swift
+++ Outline/Outline/Source/ContentView.swift
@@ -7,9 +7,32 @@
 
 import SwiftUI
 
+enum AuthState: String {
+    case onboarding
+    case logout
+    case login
+    case lookAround
+}
+
 struct ContentView: View {
+    @State private var authState: AuthState = .onboarding
     var body: some View {
-        HomeTabView()
+        Group {
+            switch authState {
+            case .onboarding:
+                LoginView()
+            case .logout:
+                LoginView()
+            case .lookAround:
+                HomeTabView()
+            case .login:
+                HomeTabView()
+            }
+        }
+        .onAppear {
+            let authStateUserDefaults = UserDefaults.standard.string(forKey: "authState")
+            self.authState = AuthState(rawValue: authStateUserDefaults ?? "onboarding") ?? .onboarding
+        }
     }
 }
 
