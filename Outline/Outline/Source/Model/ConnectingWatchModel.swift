f9f319cbd85e9366fea3338cf790d93161c0521d
Moonkey <moonkey48@icloud.com>
2023-10-22 19:29:48 +0900

    data #60 : CourseModel Target에 WatchOS 포함

--- Outline/Outline/Source/Model/ConnectingWatchModel.swift
+++ Outline/Outline/Source/Model/ConnectingWatchModel.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 struct ConnectingWatchModel {
-    private let courseModel = CourseModel()
-    
-    func readCourses() {
-        courseModel.readAllCourses { result in
-            switch result {
-            case .success(let success):
-                print(success)
-            case .failure(let failure):
-                print(failure)
-            }
-        }
-    }
+//    private let courseModel = CourseModel()
+//    
+//    func readCourses() {
+//        courseModel.readAllCourses { result in
+//            switch result {
+//            case .success(let success):
+//                print(success)
+//            case .failure(let failure):
+//                print(failure)
+//            }
+//        }
+//    }
 }
