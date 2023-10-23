37a02ccddeb8661955ba725f869113e08ea32922
Moonkey <moonkey48@icloud.com>
2023-10-23 15:32:36 +0900

    feat #63 : 위치권한 요청 뷰 구현

---
+++ Outline/Outline/Source/View/Onboarding/OnboardingLocationAuthView.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingLocationAuthView.swift
+//  Outline
+//
+//  Created by Seungui Moon on 10/23/23.
+//
+
+import CoreLocation
+import SwiftUI
+
+struct OnboardingLocationAuthView: View {
+    private let locationManager = CLLocationManager()
+
+    var body: some View {
+        VStack {
+            Text("위치 권한을 켜주세요")
+                .font(.title2)
+            Text("러닝 중 현재 위치를 정확히 알 수 있어요.")
+                .font(.subBody)
+            Spacer()
+        }
+        .onAppear {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+}
+
+#Preview {
+    OnboardingLocationAuthView()
+}
