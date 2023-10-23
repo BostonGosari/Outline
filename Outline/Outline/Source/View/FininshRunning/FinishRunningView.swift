74c3b38f5a3142ecebf2ecc8d3bd7eb15b490729
Hyebin (Helia) <hyebin218@naver.com>
2023-10-23 16:36:43 +0900

    feature #61 : 데이터 연결

--- Outline/Outline/Source/View/FininshRunning/FinishRunningView.swift
+++ Outline/Outline/Source/View/FininshRunning/FinishRunningView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct FinishRunningView: View {
-    @StateObject var viewModel = FinishRunningViewModel()
+    @StateObject private var locationManager = LocationManager()
+    @StateObject private var viewModel = FinishRunningViewModel()
     var gradientColors: [Color] = [.blackColor, .blackColor, .blackColor, .blackColor, .black50Color, .blackColor.opacity(0)]
     
     @ObservedObject var homeTabViewModel: HomeTabViewModel
+    
+    @FetchRequest (entity: CoreRunningRecord.entity(), sortDescriptors: [])
+    var runningRecord: FetchedResults<CoreRunningRecord>
 
     var body: some View {
         NavigationStack {
@@ -20,7 +24,7 @@ struct FinishRunningView: View {
                     .ignoresSafeArea()
                 VStack {
                     ZStack(alignment: .topLeading) {
-                        FinishRunningMap(userLocations: viewModel.userLocations)
+                        FinishRunningMap(userLocations: $viewModel.userLocations)
                             .roundedCorners(45, corners: .bottomLeft)
                             .shadow(color: .whiteColor, radius: 1.5)
 
@@ -67,7 +71,7 @@ struct FinishRunningView: View {
         }
         .onAppear {
             viewModel.isShowPopup = true
-            viewModel.readRunningData()
+            viewModel.readData(runningRecord: runningRecord)
         }
     }
 }
@@ -103,11 +107,11 @@ extension FinishRunningView {
         let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
         
         return LazyVGrid(columns: columns) {
-            ForEach((0...5), id: \.self) { _ in
+            ForEach(viewModel.runningData, id: \.self) { runningData in
                 VStack(alignment: .center) {
-                    Text("\(viewModel.time)")
+                    Text("\(runningData.data)")
                         .font(.title2)
-                    Text("시간")
+                    Text(runningData.text)
                         .font(.subBody)
                 }
                 .padding(.bottom, 16)
