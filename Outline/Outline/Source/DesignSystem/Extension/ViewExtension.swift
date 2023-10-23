a06388dd49dcb7a56f04518c3f10308851d40134
Moonkey <moonkey48@icloud.com>
2023-10-23 20:51:40 +0900

    feat #73 : View, UIView asImage extension method 추가

--- Outline/Outline/Source/DesignSystem/Extension/ViewExtension.swift
+++ Outline/Outline/Source/DesignSystem/Extension/ViewExtension.swift
@@ -37,4 +37,21 @@ extension View {
                 action(value)
             }
     }
+    
+    func asImage(size: CGSize) -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        controller.view.bounds = CGRect(origin: .zero, size: size)
+        let image = controller.view.asImage(size: size)
+        return image
+    }
+}
+
+extension UIView {
+    func asImage(size: CGSize) -> UIImage {
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        return UIGraphicsImageRenderer(size: size, format: format).image { context in
+            self.drawHierarchy(in: self.layer.bounds, afterScreenUpdates: true)
+        }
+    }
 }
