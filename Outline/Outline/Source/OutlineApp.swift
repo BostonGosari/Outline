e9dfe7e67c427f04a3e0324c0d0ffde035ad1bf7
Moonkey <moonkey48@icloud.com>
2023-10-23 20:49:34 +0900

    feat #73 : ShareMap 기본 render 세팅

--- Outline/Outline/Source/OutlineApp.swift
+++ Outline/Outline/Source/OutlineApp.swift
@@ -6,6 +6,7 @@
 //
 
 import Firebase
+import CoreLocation
 import SwiftUI
 
 @main
@@ -18,7 +19,14 @@ struct OutlineApp: App {
   
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ShareMainView(homeTabViewModel: HomeTabViewModel(), runningData: ShareModel(courseName: "댕댕런", runningDate: "2022년 10월", runningRegion: "세빛섬", distance: "10km", cal: "400cal", pace: "5분", bpm: "150bpm", time: "30분", userLocations: [
+                CLLocationCoordinate2D(latitude: 37.5107866, longitude: 126.9943651),
+                CLLocationCoordinate2D(latitude: 37.5107966, longitude: 126.9933751),
+                CLLocationCoordinate2D(latitude: 37.5118066, longitude: 126.9943851),
+                CLLocationCoordinate2D(latitude: 37.5118166, longitude: 126.9953951),
+                CLLocationCoordinate2D(latitude: 37.5147866, longitude: 126.9953751),
+                CLLocationCoordinate2D(latitude: 37.5137566, longitude: 126.9943751)
+            ]))
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
 
         }
