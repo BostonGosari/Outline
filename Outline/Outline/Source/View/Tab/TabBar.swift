af15ce3a7855ee911ae715b3dcff4d50ed8a4dc3
hyunjun <hyunjuntyler@gmail.com>
2023-10-25 20:05:16 +0900

    design #99 : 탭바 디테일 UI 및 인터렉션 변경

--- Outline/Outline/Source/View/Tab/TabBar.swift
+++ Outline/Outline/Source/View/Tab/TabBar.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TabBar: View {
     
     @Binding var selectedTab: Tab
-        
+    
     private let tabIconSize: CGFloat = 18
     private let tabTextSize: CGFloat = 10
     
@@ -22,8 +22,32 @@ struct TabBar: View {
                         selectedTab = item.tab
                     } label: {
                         VStack(spacing: 5) {
-                            Image(systemName: item.icon)
-                                .font(.system(size: tabIconSize))
+                            Image(item.icon)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 32)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                                        .frame(width: 32)
+                                        .foregroundStyle(.ultraThinMaterial)
+                                }
+                                .background {
+                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                        .fill(
+                                            LinearGradient(
+                                                gradient: Gradient(
+                                                    colors: [Color.primaryColor,
+                                                             Color.primaryColor.opacity(0.5)]
+                                                ),
+                                                startPoint: .bottomLeading,
+                                                endPoint: .topTrailing
+                                            )
+                                        )
+                                        .frame(width: 26)
+                                        .opacity(selectedTab == item.tab ? 1 : 0)
+                                        .rotationEffect(selectedTab == item.tab ? .degrees(15) : .degrees(0), anchor: .bottomTrailing)
+                                        .animation(.bouncy, value: selectedTab)
+                                }
                             Text(item.text)
                                 .font(.system(size: tabTextSize))
                         }
@@ -33,13 +57,16 @@ struct TabBar: View {
                     .frame(maxWidth: .infinity)
                 }
             }
-            .padding(.top, 11)
-            .padding(.bottom, 40)
+            .padding(.top, 10)
+            .padding(.bottom, 37)
             .background(
-                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
                     .foregroundStyle(.ultraThinMaterial)
-                    .shadow(color: .white, radius: 2, x: 0, y: 2)
             )
+            .background {
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                    .stroke(Color.gray400Color, lineWidth: 1)
+            }
         }
     }
 }
@@ -53,5 +80,5 @@ struct TabButtonStyle: ButtonStyle {
 }
 
 #Preview {
-    TabBar(selectedTab: .constant(Tab.GPSArtRunning))
+    HomeTabView()
 }
