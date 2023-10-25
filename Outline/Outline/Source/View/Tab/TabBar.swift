96c17f90b818381bdbebbe572dd5a88a386b8f1f
hyunjun <hyunjuntyler@gmail.com>
2023-10-25 20:15:43 +0900

    refactor #99 : 탭바 리팩토링

--- Outline/Outline/Source/View/Tab/TabBar.swift
+++ Outline/Outline/Source/View/Tab/TabBar.swift
@@ -11,63 +11,79 @@ struct TabBar: View {
     
     @Binding var selectedTab: Tab
     
-    private let tabIconSize: CGFloat = 18
-    private let tabTextSize: CGFloat = 10
-    
     var body: some View {
         VStack {
             HStack {
                 ForEach(tabItems) { item in
-                    Button {
-                        selectedTab = item.tab
-                    } label: {
-                        VStack(spacing: 5) {
-                            Image(item.icon)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 32)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                        .frame(width: 32)
-                                        .foregroundStyle(.ultraThinMaterial)
-                                }
-                                .background {
-                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
-                                        .fill(
-                                            LinearGradient(
-                                                gradient: Gradient(
-                                                    colors: [Color.primaryColor,
-                                                             Color.primaryColor.opacity(0.5)]
-                                                ),
-                                                startPoint: .bottomLeading,
-                                                endPoint: .topTrailing
-                                            )
-                                        )
-                                        .frame(width: 26)
-                                        .opacity(selectedTab == item.tab ? 1 : 0)
-                                        .rotationEffect(selectedTab == item.tab ? .degrees(15) : .degrees(0), anchor: .bottomTrailing)
-                                        .animation(.bouncy, value: selectedTab)
-                                }
-                            Text(item.text)
-                                .font(.system(size: tabTextSize))
-                        }
-                        .foregroundStyle(selectedTab == item.tab ? .primaryColor : Color("Gray400"))
-                    }
-                    .buttonStyle(TabButtonStyle())
-                    .frame(maxWidth: .infinity)
+                    TabBarButton(selectedTab: $selectedTab, item: item)
+                        .frame(maxWidth: .infinity)
                 }
             }
-            .padding(.top, 10)
-            .padding(.bottom, 37)
+            .padding(.top, 11)
+            .padding(.bottom, 34)
             .background(
                 RoundedRectangle(cornerRadius: 15, style: .continuous)
                     .foregroundStyle(.ultraThinMaterial)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15, style: .continuous)
+                            .stroke(Color.gray400Color, lineWidth: 1)
+                    )
             )
-            .background {
-                RoundedRectangle(cornerRadius: 15, style: .continuous)
-                    .stroke(Color.gray400Color, lineWidth: 1)
+        }
+    }
+}
+
+struct TabBarButton: View {
+    
+    @Binding var selectedTab: Tab
+    let item: TabItem
+    
+    var body: some View {
+        Button {
+            selectedTab = item.tab
+        } label: {
+            VStack(spacing: 5) {
+                TabBarIcon(selectedTab: $selectedTab, item: item)
+                Text(item.text)
+                    .font(.caption2)
+                    .foregroundColor(selectedTab == item.tab ? Color.primaryColor : Color("Gray400"))
             }
         }
+        .buttonStyle(TabButtonStyle())
+    }
+}
+
+struct TabBarIcon: View {
+    
+    @Binding var selectedTab: Tab
+    let item: TabItem
+    
+    var body: some View {
+        Image(item.icon)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 32)
+            .background {
+                RoundedRectangle(cornerRadius: 12, style: .continuous)
+                    .frame(width: 32)
+                    .foregroundStyle(.ultraThinMaterial)
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [Color.primaryColor, Color.primaryColor.opacity(0.5)]
+                            ),
+                            startPoint: .bottomLeading,
+                            endPoint: .topTrailing
+                        )
+                    )
+                    .frame(width: 26)
+                    .opacity(selectedTab == item.tab ? 1 : 0)
+                    .rotationEffect(selectedTab == item.tab ? .degrees(15) : .degrees(0), anchor: .bottomTrailing)
+                    .animation(.bouncy, value: selectedTab)
+            }
     }
 }
 
@@ -75,7 +91,7 @@ struct TabButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .scaleEffect(configuration.isPressed ? 0.97 : 1)
-            .animation(.easeInOut, value: configuration.isPressed)
+            .animation(.easeInOut(duration: 0.2), value: configuration.isPressed)
     }
 }
 
