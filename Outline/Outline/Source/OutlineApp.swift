9bac2a395297cb5e7ad74dc3c5b2bae72ff9fe9d
Moonkey <moonkey48@icloud.com>
2023-10-27 01:12:53 +0900

    feat #122 : photoSelectView 비율 수정

--- Outline/Outline/Source/OutlineApp.swift
+++ Outline/Outline/Source/OutlineApp.swift
@@ -19,15 +19,7 @@ struct OutlineApp: App {
   
     var body: some Scene {
         WindowGroup {
-            ShareMainView(homeTabViewModel: HomeTabViewModel(), runningData: ShareModel(courseName: "댕댕런", runningDate: "", runningRegion: "", distance: "29KM", cal: "500cal", pace: "5'55", bpm: "150bpm", time: "2시간", userLocations: [
-                CLLocationCoordinate2D(latitude: 36, longitude: 129),
-                CLLocationCoordinate2D(latitude: 36.001, longitude: 129.004),
-                CLLocationCoordinate2D(latitude: 36.003, longitude: 129.010),
-                CLLocationCoordinate2D(latitude: 36.009, longitude: 129.001),
-                CLLocationCoordinate2D(latitude: 35.990, longitude: 128.001),
-                CLLocationCoordinate2D(latitude: 36.031, longitude: 129.304)
-                
-            ]))
+            ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
 
         }
