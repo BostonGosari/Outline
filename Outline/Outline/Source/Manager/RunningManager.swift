46e935bc7b5ae232b9cda224e95f5fa845090d51
hyunjun <hyunjuntyler@gmail.com>
2023-10-26 23:28:22 +0900

    feat #115 : 위치기반 GPS 아트러닝 시작 판별기능 추가

--- Outline/Outline/Source/Manager/RunningManager.swift
+++ Outline/Outline/Source/Manager/RunningManager.swift
@@ -53,4 +53,40 @@ class RunningManager: ObservableObject {
             }
         }
     }
+    
+    func checkDistance(userLocation: CLLocationCoordinate2D, course: [Coordinate]) -> Bool {
+        guard let shortestDistance = calculateShortestDistance(from: userLocation, to: convertToCLLocationCoordinates(course)) else {
+            print("can't find user location")
+            return false
+        }
+        return shortestDistance <= 50.0
+    }
+}
+
+// MARK: - 위치와 경로를 계산하는 함수
+
+extension RunningManager {
+
+    func calculateShortestDistance(from userCoordinate: CLLocationCoordinate2D, to courseCoordinates: [CLLocationCoordinate2D]) -> CLLocationDistance? {
+        guard !courseCoordinates.isEmpty else { return nil }
+
+        var shortestDistance: CLLocationDistance?
+        
+        for courseCoordinate in courseCoordinates {
+            let distanceToCourseCoordinate = calculateDistance(from: userCoordinate, to: courseCoordinate)
+            if let currentShortestDistance = shortestDistance {
+                shortestDistance = min(currentShortestDistance, distanceToCourseCoordinate)
+            } else {
+                shortestDistance = distanceToCourseCoordinate
+            }
+        }
+        return shortestDistance
+    }
+
+    // 두 좌표 사이의 거리를 계산하는 함수
+    private func calculateDistance(from coordinate1: CLLocationCoordinate2D, to coordinate2: CLLocationCoordinate2D) -> CLLocationDistance {
+        let location1 = CLLocation(latitude: coordinate1.latitude, longitude: coordinate1.longitude)
+        let location2 = CLLocation(latitude: coordinate2.latitude, longitude: coordinate2.longitude)
+        return location1.distance(from: location2)
+    }
 }
