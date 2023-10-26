b0f738cfff657bd68e21a4831a0994b82d3832fa
hyunjun <hyunjuntyler@gmail.com>
2023-10-27 01:58:58 +0900

    design #115 : 러닝뷰 UI 수정

--- Outline/Outline/Source/View/Running/RunningView.swift
+++ Outline/Outline/Source/View/Running/RunningView.swift
@@ -15,6 +15,7 @@ struct RunningView: View {
     @StateObject var digitalTimerViewModel = DigitalTimerViewModel()
     @StateObject var locationManager = LocationManager()
     
+    @State var checkRunning = true
     @State var selection = 0
     
     init(homeTabViewModel: HomeTabViewModel) {
@@ -44,47 +45,49 @@ struct RunningView: View {
                     }
                 }
                 
-                ZStack {
-                    if !locationManager.checkDistance {
-                        Color.black.opacity(0.5)
-                    }
-                    VStack(spacing: 10) {
-                        Text("자유코스로 변경할까요?")
-                            .font(.title2)
-                        Text("앗! 현재 루트와 멀리 떨어져 있어요.")
-                            .font(.subBody)
-                            .foregroundColor(.gray300Color)
-                        Image("AnotherLocation")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 120)
-                        Button {
-                            runningManager.startFreeRun()
-                        } label: {
-                            Text("자유코스로 변경하기")
-                                .font(.button)
-                                .foregroundStyle(Color.blackColor)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
-                                        .foregroundStyle(Color.primaryColor)
-                                }
+                    ZStack {
+                        if !locationManager.checkDistance && checkRunning {
+                            Color.black.opacity(0.5)
                         }
-                        .padding()
-                    }
-                    .frame(height: UIScreen.main.bounds.height / 2)
-                    .frame(maxWidth: .infinity)
-                    .background {
-                        RoundedRectangle(cornerRadius: 30, style: .continuous)
-                            .stroke(Color.primaryColor, lineWidth: 2)
-                        RoundedRectangle(cornerRadius: 30, style: .continuous)
-                            .foregroundStyle(Color.gray900Color)
-                    }
-                    .frame(maxHeight: .infinity, alignment: .bottom)
-                    .transition(.move(edge: .bottom))
+                        VStack(spacing: 10) {
+                            Text("자유코스로 변경할까요?")
+                                .font(.title2)
+                            Text("앗! 현재 루트와 멀리 떨어져 있어요.")
+                                .font(.subBody)
+                                .foregroundColor(.gray300Color)
+                            Image("AnotherLocation")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 120)
+                            Button {
+                                runningManager.startFreeRun()
+                                checkRunning = false
+                            } label: {
+                                Text("자유코스로 변경하기")
+                                    .font(.button)
+                                    .foregroundStyle(Color.blackColor)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                            .foregroundStyle(Color.primaryColor)
+                                    }
+                            }
+                            .padding()
+                        }
+                        .frame(height: UIScreen.main.bounds.height / 2)
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                                .stroke(Color.primaryColor, lineWidth: 2)
+                            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                                .foregroundStyle(Color.gray900Color)
+                        }
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .offset(y: !locationManager.checkDistance && checkRunning ? 0 : UIScreen.main.bounds.height / 2 + 2)
+                        .animation(.easeInOut, value: locationManager.checkDistance)
+                    .ignoresSafeArea()
                 }
-                .ignoresSafeArea()
             }
         }
     }
