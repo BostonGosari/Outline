e6de0192815d2beb03cd52a560972fdcc7d5f0a6
Moonkey <moonkey48@icloud.com>
2023-10-26 16:19:48 +0900

    fix #104 : 사진 포함 공유시 사진 비율 오류 해결

--- Outline/Outline/Source/OutlineApp.swift
+++ Outline/Outline/Source/OutlineApp.swift
@@ -19,7 +19,7 @@ struct OutlineApp: App {
   
     var body: some Scene {
         WindowGroup {
-            ShareMainView(homeTabViewModel: HomeTabViewModel(), runningData: ShareModel(courseName: "댕댕런", runningDate: "2022.10.23", runningRegion: "효자동", distance: "5.2km", cal: "400cal", pace: "5.5", bpm: "150bpm", time: "30분20초", userLocations: [
+            ShareMainView(homeTabViewModel: HomeTabViewModel(), runningData: ShareModel(courseName: "댕댕런", runningDate: "2022.10.23", runningRegion: "효자동", distance: "5.2KM", cal: "400cal", pace: "5'5''", bpm: "150bpm", time: "30분20초", userLocations: [
                 CLLocationCoordinate2D(latitude: 36, longitude: 129),
                 CLLocationCoordinate2D(latitude: 36.0214, longitude: 129.1),
                 CLLocationCoordinate2D(latitude: 36.214, longitude: 129.1),
