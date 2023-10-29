a55f0226afa1585283fb6a7fdfe7e13f3c4abcc3
Moonkey <moonkey48@icloud.com>
2023-10-29 13:10:24 +0900

    feat #139 : Apple Login 기능 구현 완료

--- Outline/Outline/Source/ViewModel/Login/FirebaseAuthViewModel.swift
+++ Outline/Outline/Source/ViewModel/Login/FirebaseAuthViewModel.swift
@@ -7,7 +7,12 @@
 
 import SwiftUI
 
-
 class FirebaseAuthViewModel: ObservableObject {
+    @Published private var appleLoginCoordinator: AppleAuthCoordinator?
     
+    func handleAppleLogin(window: UIWindow?) {
+        appleLoginCoordinator = AppleAuthCoordinator(window: window)
+        appleLoginCoordinator?.startAppleLogin()
+    }
+
 }
