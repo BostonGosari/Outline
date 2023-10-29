b607c7c144c7d07a0d21150b57ac33cbe1b86dba
chamroro <chamsae@handong.ac.kr>
2023-10-29 14:45:19 +0900

    chore : 들여쓰기 수정

--- Outline/Outline/Source/View/GPSArtHome/BottomScrollView.swift
+++ Outline/Outline/Source/View/GPSArtHome/BottomScrollView.swift
@@ -16,68 +16,66 @@ struct BottomScrollView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("이런 코스도 있어요.")
-                .font(Font.system(size: 20).weight(.semibold))
+                .font(.subtitle)
                 .foregroundColor(.white)
-     
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                    ForEach(homeTabViewModel.withoutRecommendedCourses, id: \.id) { currentCourse in
-                        ZStack {
-                            Button {
-                                selectedCourse = currentCourse
-                            } label: {
-                                AsyncImage(url: URL(string: currentCourse.course.thumbnail)) { image in
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                } placeholder: {
-                                    Rectangle()
-                                }
-                            
-                                .overlay {
-                                    LinearGradient(
-                                        stops: [
-                                            Gradient.Stop(color: .black, location: 0.00),
-                                            Gradient.Stop(color: .black.opacity(0), location: 1.00)
-                                        ],
-                                        startPoint: UnitPoint(x: 0.5, y: 0.9),
-                                        endPoint: UnitPoint(x: 0.5, y: 0.1)
-                                    )
-                                    
-                                }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                ForEach(homeTabViewModel.withoutRecommendedCourses, id: \.id) { currentCourse in
+                    ZStack {
+                        Button {
+                            selectedCourse = currentCourse
+                        } label: {
+                            AsyncImage(url: URL(string: currentCourse.course.thumbnail)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                Rectangle()
                             }
-                            VStack(alignment: .leading, spacing: 4) {
-                                Spacer()
-                                Text("\(currentCourse.course.courseName)")
-                                    .font(Font.system(size: 20).weight(.semibold))
-                                    .foregroundColor(.white)
-                                HStack(spacing: 0) {
-                                    Image(systemName: "mappin")
-                                        .foregroundColor(.gray600)
-                                    Text("\(currentCourse.course.locationInfo.locality) \(currentCourse.course.locationInfo.subLocality)")
-                                        .foregroundColor(.gray600)
-                                }
-                                .font(.caption)
-                                .padding(.bottom, 21)
+                            .overlay {
+                                LinearGradient(
+                                    stops: [
+                                        Gradient.Stop(color: .black, location: 0.00),
+                                        Gradient.Stop(color: .black.opacity(0), location: 1.00)
+                                    ],
+                                    startPoint: UnitPoint(x: 0.5, y: 0.9),
+                                    endPoint: UnitPoint(x: 0.5, y: 0.1)
+                                )
+                                
                             }
-                            .frame(width: 164, height: 235)
-                            .offset(x: -20)
                         }
-                        .frame(width: 164, height: 236)
-                        .roundedCorners(5, corners: [.topLeft])
-                        .roundedCorners(30, corners: [.topRight, .bottomLeft, .bottomRight])
-                        .fullScreenCover(item: $selectedCourse) { course in
-                            CourseDetailView(homeTabViewModel: homeTabViewModel, course: course)
+                        VStack(alignment: .leading, spacing: 4) {
+                            Spacer()
+                            Text("\(currentCourse.course.courseName)")
+                                .font(Font.system(size: 20).weight(.semibold))
+                                .foregroundColor(.white)
+                            HStack(spacing: 0) {
+                                Image(systemName: "mappin")
+                                    .foregroundColor(.gray600)
+                                Text("\(currentCourse.course.locationInfo.locality) \(currentCourse.course.locationInfo.subLocality)")
+                                    .foregroundColor(.gray600)
+                            }
+                            .font(.caption)
+                            .padding(.bottom, 21)
                         }
-                        .frame(width: 164, height: 237)
-                        .shadow(color: .white, radius: 0.5, x: 0, y: -0.5)
-                        .shadow(color: .white, radius: 0.5, x: 0.5, y: 0)
-                        .shadow(color: .white, radius: 0.5, x: 0, y: 0.5)
-                        .shadow(color: .white, radius: 0.5, x: -0.5, y: 0)
+                        .frame(width: 164, height: 235)
+                        .offset(x: -20)
                     }
+                    .frame(width: 164, height: 236)
+                    .roundedCorners(5, corners: [.topLeft])
+                    .roundedCorners(30, corners: [.topRight, .bottomLeft, .bottomRight])
+                    .fullScreenCover(item: $selectedCourse) { course in
+                        CourseDetailView(homeTabViewModel: homeTabViewModel, course: course)
+                    }
+                    .frame(width: 164, height: 237)
+                    .shadow(color: .white, radius: 0.5, x: 0, y: -0.5)
+                    .shadow(color: .white, radius: 0.5, x: 0.5, y: 0)
+                    .shadow(color: .white, radius: 0.5, x: 0, y: 0.5)
+                    .shadow(color: .white, radius: 0.5, x: -0.5, y: 0)
                 }
-                .frame(height: 238)
             }
+            .frame(height: 238)
+        }
             .padding(.top, 10)
             .padding(.bottom, 106)
         }
@@ -94,14 +92,12 @@ struct BottomScrollView: View {
 }
 
 struct CourseDetailView: View {
-    
     @ObservedObject var homeTabViewModel: HomeTabViewModel
-    var course: CourseWithDistance
     @Environment(\.dismiss) var dismiss
     @StateObject var runningManager = RunningManager.shared
-    
     @State private var isUnlocked = false
     @State private var showAlert = false
+    var course: CourseWithDistance
     
     var body: some View {
         ZStack {
@@ -351,12 +347,6 @@ struct CourseBannerView: View {
                 .padding(-10)
         }
         .padding(40)
-    }
-}
-
-struct BottomScrollDetailView: View {
-    var body: some View {
-        Text("1")
     }
 }
 
