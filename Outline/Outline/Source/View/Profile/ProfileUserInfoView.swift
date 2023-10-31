23f755f02a60f84908198362ab2a952a2939b6cc
Moonkey <moonkey48@icloud.com>
2023-10-31 23:51:35 +0900

    design #155 : Profile View 디자인 구현

---
+++ Outline/Outline/Source/View/Profile/ProfileUserInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileUserInfoView.swift
+//  Outline
+//
+//  Created by Seungui Moon on 10/31/23.
+//
+
+import SwiftUI
+
+struct ProfileUserInfoView: View {
+    var body: some View {
+        Text("User Info View")
+    }
+}
+
+#Preview {
+    ProfileUserInfoView()
+}
