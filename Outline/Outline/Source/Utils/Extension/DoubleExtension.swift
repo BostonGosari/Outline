bdffedc5df27c97f57beed44b5ac35b8e7180ef0
hyunjun <hyunjuntyler@gmail.com>
2023-11-02 04:30:27 +0900

    design #186 : UI 수정, 변경사항 반영

--- Outline/Outline/Source/Utils/Extension/DoubleExtension.swift
+++ Outline/Outline/Source/Utils/Extension/DoubleExtension.swift
@@ -14,7 +14,7 @@ extension Double {
         }
         let minutes = Int(self / 60)
         let seconds = Int(self) % 60
-        return String(format: "%02d'%02d''", minutes, seconds)
+        return String(format: "%d'%02d''", minutes, seconds)
     }
     
     func formatDuration() -> String {
