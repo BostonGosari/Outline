c3a589416642eff0e6fa3805f35e737d2142b152
Moonkey <moonkey48@icloud.com>
2023-11-02 03:29:51 +0900

    feat #184 : GPSArtHomeView에 lookaround popup 추가 및 modal 에러 수정

--- Outline/Outline/Source/View/LookAround/LookAroundView.swift
+++ Outline/Outline/Source/View/LookAround/LookAroundView.swift
@@ -47,10 +47,13 @@ struct LookAroundModalView: View {
                     .padding(.bottom, 8)
                 Text("OUTLINE에 가입하면 내가 그린\n러닝 기록을 확인할 수 있어요!")
                     .font(.subBody)
-                    .padding(.bottom, 32)
+                    
                     .multilineTextAlignment(.center)
                     .foregroundStyle(.gray300)
                 Image("lookAroundImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 152, height: 152)
                     .offset(x: 22)
                 
                 CompleteButton(text: "가입하러 가기", isActive: true) {
