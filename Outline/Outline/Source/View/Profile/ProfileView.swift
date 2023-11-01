73ada72f20d2fa2987432e841c2804def1a83dc9
Moonkey <moonkey48@icloud.com>
2023-11-01 16:31:33 +0900

    feat #155 : ProfileView 계정 삭제, 로그아웃 alert 설정

--- Outline/Outline/Source/View/Profile/ProfileView.swift
+++ Outline/Outline/Source/View/Profile/ProfileView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var showDeleteUserAlert = false
+    @State private var showDeleteCompleteAlert = false
+    @State private var showLogoutAlert = false
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -52,16 +56,39 @@ struct ProfileView: View {
                 List {
                     Group {
                         Button {
-                            // signout
+                            showDeleteUserAlert = true
                         } label: {
                             Text("계정 삭제")
                                 .foregroundStyle(Color.customRed)
                         }
+                        .alert(isPresented: $showDeleteUserAlert) {
+                            Alert(
+                                title: Text("계정 삭제"),
+                                message: Text("이별인가요? 너무아쉬워요.\n 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
+                                primaryButton: .default(Text("취소"), action: {
+                                    showDeleteUserAlert = false
+                                }), secondaryButton: .default(Text("삭제").bold(), action: {
+                                    showDeleteUserAlert = false
+                                    // handle delete User
+                                    showDeleteCompleteAlert = true
+                                }))
+                        }
 
                         Button {
-                            // logout
+                            showLogoutAlert = true
                         } label: {
                             Text("로그아웃")
+                        }
+                        .alert(isPresented: $showLogoutAlert) {
+                            Alert(
+                                title: Text("로그아웃"),
+                                message: Text("정말 로그아웃 하시겠나요?"),
+                                primaryButton: .default(Text("닫기"), action: {
+                                    showLogoutAlert = false
+                                }), secondaryButton: .default(Text("로그아웃").bold(), action: {
+                                    showLogoutAlert = false
+                                    // handle logout
+                                }))
                         }
 
                     }
@@ -78,6 +105,17 @@ struct ProfileView: View {
             .navigationTitle("설정")
             .navigationBarTitleDisplayMode(.inline)
             .background(Color.gray900)
+            .alert(isPresented: $showDeleteCompleteAlert) {
+                Alert(
+                    title: Text("탈퇴 완료"),
+                    message: Text("탈퇴 처리가 성공적으로\n 완료되었습니다."),
+                    primaryButton: .default(Text("닫기"), action: {
+                        showDeleteCompleteAlert = false
+                    }), secondaryButton: .default(Text("확인").bold(), action: {
+                        showDeleteCompleteAlert = false
+                        // handle navigate to Login View
+                    }))
+            }
         }
         .tint(Color.customPrimary)
     }
