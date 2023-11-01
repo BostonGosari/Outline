5a11ad1d3915b43c90936708460cc94cce64a148
hyunjun <hyunjuntyler@gmail.com>
2023-11-01 18:47:21 +0900

    feat #167 : 워치 러닝 매니저 생성

---
+++ Outline/Outline Watch/Source/Manager/WatchRunningManager.swift
@@ -0,0 +1,86 @@
+//
+//  WatchRunningManager.swift
+//  Outline Watch App
+//
+//  Created by hyunjun on 11/1/23.
+//
+
+import CoreLocation
+
+class WatchRunningManager: ObservableObject {
+    
+    static var shared = WatchRunningManager()
+    
+    private init() { }
+    
+    @Published var userLocations: [CLLocationCoordinate2D] = []
+    @Published var changeRunningType = false
+    
+    private let locationManager = CLLocationManager()
+    var startCourse: GPSArtCourse = GPSArtCourse()
+    var runningType: RunningType = .gpsArt
+    
+    func startFreeRun() {
+        startCourse = GPSArtCourse()
+        runningType = .free
+        getFreeRunName()
+    }
+    
+    func startGPSArtRun() {
+        runningType = .gpsArt
+    }
+    
+    func trackingDistance() {
+        if !checkDistance(course: startCourse.coursePaths) {
+            changeRunningType = true
+        }
+    }
+    
+    func checkDistance(course: [Coordinate]) -> Bool {
+        
+        guard let userLocation = locationManager.location?.coordinate else { return false }
+        
+        guard let shortestDistance = calculateShortestDistance(from: userLocation, to: ConvertCoordinateManager.convertToCLLocationCoordinates(course)) else { return false }
+        return shortestDistance <= 2000
+    }
+    
+    private func calculateShortestDistance(from userCoordinate: CLLocationCoordinate2D, to courseCoordinates: [CLLocationCoordinate2D]) -> CLLocationDistance? {
+        guard !courseCoordinates.isEmpty else { return nil }
+        
+        var shortestDistance: CLLocationDistance?
+        
+        for courseCoordinate in courseCoordinates {
+            let distanceToCourseCoordinate = calculateDistance(from: userCoordinate, to: courseCoordinate)
+            if let currentShortestDistance = shortestDistance {
+                shortestDistance = min(currentShortestDistance, distanceToCourseCoordinate)
+            } else {
+                shortestDistance = distanceToCourseCoordinate
+            }
+        }
+        return shortestDistance
+    }
+    
+    private func calculateDistance(from coordinate1: CLLocationCoordinate2D, to coordinate2: CLLocationCoordinate2D) -> CLLocationDistance {
+        let location1 = CLLocation(latitude: coordinate1.latitude, longitude: coordinate1.longitude)
+        let location2 = CLLocation(latitude: coordinate2.latitude, longitude: coordinate2.longitude)
+        return location1.distance(from: location2)
+    }
+    
+    private func getFreeRunName() {
+        let geocoder = CLGeocoder()
+        
+        if let userLocation = locationManager.location?.coordinate {
+            let start = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+            geocoder.reverseGeocodeLocation(start) { placemarks, error in
+                if let error = error {
+                    print("Reverse geocoding error: \(error.localizedDescription)")
+                } else if let placemark = placemarks?.first {
+                    let city = placemark.locality ?? ""
+                    let town = placemark.subLocality ?? ""
+                    
+                    self.startCourse.courseName = "\(city) \(town)런"
+                }
+            }
+        }
+    }
+}
