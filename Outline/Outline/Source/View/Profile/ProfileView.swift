840c180bcc050a2984678045f03a10a4fc8cd715
Moonkey <moonkey48@icloud.com>
2023-11-01 16:36:57 +0900

    chore #155 : 공동 코드 영역 복원 및 코드 정리

--- Outline/Outline/Source/View/Profile/ProfileView.swift
+++ Outline/Outline/Source/View/Profile/ProfileView.swift
@@ -64,7 +64,7 @@ struct ProfileView: View {
                         .alert(isPresented: $showDeleteUserAlert) {
                             Alert(
                                 title: Text("계정 삭제"),
-                                message: Text("이별인가요? 너무아쉬워요.\n 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
+                                message: Text("이별인가요? 너무아쉬워요. 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
                                 primaryButton: .default(Text("취소"), action: {
                                     showDeleteUserAlert = false
                                 }), secondaryButton: .default(Text("삭제").bold(), action: {
@@ -108,13 +108,14 @@ struct ProfileView: View {
             .alert(isPresented: $showDeleteCompleteAlert) {
                 Alert(
                     title: Text("탈퇴 완료"),
-                    message: Text("탈퇴 처리가 성공적으로\n 완료되었습니다."),
+                    message: Text("탈퇴 처리가 성공적으로 완료되었습니다."),
                     primaryButton: .default(Text("닫기"), action: {
                         showDeleteCompleteAlert = false
                     }), secondaryButton: .default(Text("확인").bold(), action: {
                         showDeleteCompleteAlert = false
                         // handle navigate to Login View
-                    }))
+                    })
+                )
             }
         }
         .tint(Color.customPrimary)
