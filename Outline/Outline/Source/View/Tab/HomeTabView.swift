6313c3d957d13c41f72d6f08e92310aba38f8e64
chamroro <chamsae@handong.ac.kr>
2023-11-02 12:32:00 +0900

    design #178 : 시트 UI 업로드

--- Outline/Outline/Source/View/Tab/HomeTabView.swift
+++ Outline/Outline/Source/View/Tab/HomeTabView.swift
@@ -14,7 +14,6 @@ struct HomeTabView: View {
     @State private var selectedTab: Tab = .GPSArtRunning
     @State private var showDetailView = false
     @State private var showCustomSheet = false
-    
     var body: some View {
         ZStack {
             NavigationStack {
@@ -45,9 +44,7 @@ struct HomeTabView: View {
                             .frame(maxHeight: .infinity, alignment: .top)
                     }
                 }
-                .sheet(isPresented: $showCustomSheet) {
-                    WatchRunningSheet
-                }
+             
             }
             if runningManager.start {
                 CountDown(running: $runningManager.running, start: $runningManager.start)
@@ -55,6 +52,10 @@ struct HomeTabView: View {
             if runningManager.running {
                 RunningView()
             }
+            if showCustomSheet {
+                WatchRunningSheet
+            }
+                
         }
         .onReceive(watchConnectivityManager.$isWatchRunning) { isRunning in
               if isRunning {
@@ -67,39 +68,57 @@ struct HomeTabView: View {
 
 extension HomeTabView {
     private var WatchRunningSheet: some View {
-        VStack(spacing: 0) {
-            Text("Apple Watch 러닝")
-                .font(.title2)
-                .padding(.top, 72)
-                .padding(.bottom, 35)
-                .foregroundStyle(Color.customWhite)
-            Text("지금은 Apple Watch로 러닝을 추적하고 있어요.\n러닝 중에 휴대폰으로 러닝 데이터를 확인하려면\n휴대폰에서 러닝을 시작해야 합니다.\n\n※휴대폰에서 러닝을 시작하면 별개의\n새로운 러닝이 시작됩니다.")
-                .multilineTextAlignment(.center)
-                .font(.subBody)
-                .padding(.bottom, 24)
-                .foregroundStyle(Color.gray400)
-            Button {
-                showCustomSheet.toggle()
-            } label: {
-                Text("완료")
-                    .font(.button)
-                    .foregroundStyle(Color.customBlack)
-                    .padding()
-                    .frame(maxWidth: .infinity, maxHeight: 55)
-                    .background {
-                        RoundedRectangle(cornerRadius: 15, style: .continuous)
-                            .foregroundStyle(Color.customPrimary)
+        ZStack {
+            Color.black.opacity(0.5)
+                .onTapGesture {
+                    withAnimation {
+                        showCustomSheet = false
                     }
+                }
+            VStack(spacing: 0) {
+                Text("Apple Watch로 러닝을\n추적하고 있어요")
+                    .font(.title2)
+                    .padding(.top, 44)
+                    .padding(.bottom, 20)
+                    .foregroundStyle(Color.customWhite)
+                    .multilineTextAlignment(.center)
+                Image("RunningWatch")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .padding(.bottom, 16)
+                
+                Text("휴대폰에서 러닝을 시작하면 별개의\n새로운 러닝이 시작됩니다.")
+                    .multilineTextAlignment(.center)
+                    .font(.subBody)
+                    .padding(.bottom, 30)
+                    .foregroundStyle(Color.gray400)
+                Button {
+                    showCustomSheet.toggle()
+                } label: {
+                    Text("완료")
+                        .font(.button)
+                        .foregroundStyle(Color.customBlack)
+                        .padding()
+                        .frame(maxWidth: .infinity, maxHeight: 55)
+                        .background {
+                            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                                .foregroundStyle(Color.customPrimary)
+                        }
+                }
+                .padding()
             }
-            .padding()
-            .padding(.top, 87)
+            .frame(height: UIScreen.main.bounds.height / 2)
+            .frame(maxWidth: .infinity)
+            .background {
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .stroke(Color.customPrimary, lineWidth: 2)
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .foregroundStyle(Color.gray900)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .offset(y: showCustomSheet ? 0 : UIScreen.main.bounds.height / 2 + 2)
+            .animation(.easeInOut, value: showCustomSheet)
+            .ignoresSafeArea()
         }
-        .presentationDetents([.height(448)])
-        .presentationCornerRadius(35)
-        .interactiveDismissDisabled()
     }
-}
-
-#Preview {
-    HomeTabView()
 }
