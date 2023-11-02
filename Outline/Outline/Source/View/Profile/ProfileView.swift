5a9607c069bd62c73268fb2700c2da4d85a847c4
Moonkey <moonkey48@icloud.com>
2023-11-03 00:48:12 +0900

    feat #205 : 둘러보기에서 profile 클릭시 앱 종료 에러 해결

--- Outline/Outline/Source/View/Profile/ProfileView.swift
+++ Outline/Outline/Source/View/Profile/ProfileView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @Environment(\.dismiss) private var dismiss
     @AppStorage("authState") var authState: AuthState = .logout
     @ObservedObject private var profileViewModel = ProfileViewModel()
     
@@ -19,120 +20,152 @@ struct ProfileView: View {
     @State private var userProfileImage = "defaultProfileImage"
     
     var body: some View {
-        VStack {
-            Image(userProfileImage)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 118, height: 118)
-                .padding(.top, 18)
-            
-            Text(profileViewModel.userInfo.nickname)
-            Divider()
-                .frame(height: 1)
-                .background(Color.gray700)
-                .padding(.top, 20)
-            List {
-                Group {
-                    NavigationLink {
-                        ProfileUserInfoView(nickname: $profileViewModel.userInfo.nickname, birthday: $profileViewModel.userInfo.birthday, gender: $profileViewModel.userInfo.gender) {
-                            profileViewModel.saveUserInfoOnDB()
+        ZStack {
+            VStack {
+                Image(userProfileImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 118, height: 118)
+                    .padding(.top, 18)
+                
+                Text(profileViewModel.userInfo.nickname)
+                Divider()
+                    .frame(height: 1)
+                    .background(Color.gray700)
+                    .padding(.top, 20)
+                List {
+                    Group {
+                        NavigationLink {
+                            ProfileUserInfoView(nickname: $profileViewModel.userInfo.nickname, birthday: $profileViewModel.userInfo.birthday, gender: $profileViewModel.userInfo.gender) {
+                                profileViewModel.saveUserInfoOnDB()
+                            }
+                        } label: {
+                            Text("내 정보")
+                                .padding(.vertical, 5)
                         }
-                    } label: {
-                        Text("내 정보")
-                            .padding(.vertical, 5)
-                    }
-                    NavigationLink {
-                        ProfileHealthInfoView(
-                            height: $profileViewModel.userInfo.height,
-                            weight: $profileViewModel.userInfo.weight
-                        ) {
-                            profileViewModel.saveUserInfoOnDB()
+                        NavigationLink {
+                            ProfileHealthInfoView(
+                                height: $profileViewModel.userInfo.height,
+                                weight: $profileViewModel.userInfo.weight
+                            ) {
+                                profileViewModel.saveUserInfoOnDB()
+                            }
+                        } label: {
+                            Text("신체 정보")
+                                .padding(.vertical, 5)
                         }
-                    } label: {
-                        Text("신체 정보")
-                            .padding(.vertical, 5)
                     }
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
                 }
-                .listRowBackground(Color.clear)
-                .listRowSeparator(.hidden)
-            }
-            .environment(\.defaultMinListRowHeight, 50)
-            .listStyle(.plain)
-            .frame(height: 100)
-            .padding(.top, 10)
-            
-            Rectangle()
-                .fill(Color.gray800)
-                .frame(height: 8)
-                .padding(.top, 20)
-            List {
-                Group {
-                    Button {
-                        showDeleteUserAlert = true
-                    } label: {
-                        Text("계정 삭제")
-                            .foregroundStyle(Color.customRed)
-                    }
-                    .alert(isPresented: $showDeleteUserAlert) {
-                        Alert(
-                            title: Text("계정 삭제"),
-                            message: Text("이별인가요? 너무아쉬워요. 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
-                            primaryButton: .default(Text("취소"), action: {
-                                showDeleteUserAlert = false
-                            }), secondaryButton: .default(Text("삭제").bold(), action: {
-                                showDeleteUserAlert = false
-                                profileViewModel.signOut()
-                                showDeleteCompleteAlert = true
-                            }))
-                    }
+                .environment(\.defaultMinListRowHeight, 50)
+                .listStyle(.plain)
+                .frame(height: 100)
+                .padding(.top, 10)
+                
+                Rectangle()
+                    .fill(Color.gray800)
+                    .frame(height: 8)
+                    .padding(.top, 20)
+                List {
+                    Group {
+                        Button {
+                            showDeleteUserAlert = true
+                        } label: {
+                            Text("계정 삭제")
+                                .foregroundStyle(Color.customRed)
+                        }
+                        .alert(isPresented: $showDeleteUserAlert) {
+                            Alert(
+                                title: Text("계정 삭제"),
+                                message: Text("이별인가요? 너무아쉬워요. 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
+                                primaryButton: .default(Text("취소"), action: {
+                                    showDeleteUserAlert = false
+                                }), secondaryButton: .default(Text("삭제").bold(), action: {
+                                    showDeleteUserAlert = false
+                                    profileViewModel.signOut()
+                                    showDeleteCompleteAlert = true
+                                }))
+                        }
 
-                    Button {
-                        showLogoutAlert = true
-                    } label: {
-                        Text("로그아웃")
-                    }
-                    .alert(isPresented: $showLogoutAlert) {
-                        Alert(
-                            title: Text("로그아웃"),
-                            message: Text("정말 로그아웃 하시겠나요?"),
-                            primaryButton: .default(Text("닫기"), action: {
-                                showLogoutAlert = false
-                            }), secondaryButton: .default(Text("로그아웃").bold(), action: {
-                                showLogoutAlert = false
-                                profileViewModel.logOut()
-                                // handle logout
-                            }))
-                    }
+                        Button {
+                            showLogoutAlert = true
+                        } label: {
+                            Text("로그아웃")
+                        }
+                        .alert(isPresented: $showLogoutAlert) {
+                            Alert(
+                                title: Text("로그아웃"),
+                                message: Text("정말 로그아웃 하시겠나요?"),
+                                primaryButton: .default(Text("닫기"), action: {
+                                    showLogoutAlert = false
+                                }), secondaryButton: .default(Text("로그아웃").bold(), action: {
+                                    showLogoutAlert = false
+                                    profileViewModel.logOut()
+                                    // handle logout
+                                }))
+                        }
 
+                    }
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
                 }
-                .listRowBackground(Color.clear)
-                .listRowSeparator(.hidden)
+                .environment(\.defaultMinListRowHeight, 50)
+                .listStyle(.plain)
+                .frame(height: 150)
+                .padding(.top, 10)
+                
+                Spacer()
             }
-            .environment(\.defaultMinListRowHeight, 50)
-            .listStyle(.plain)
-            .frame(height: 150)
-            .padding(.top, 10)
+            .navigationTitle("설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color.gray900)
+            .alert(isPresented: $showDeleteCompleteAlert) {
+                Alert(
+                    title: Text("탈퇴 완료"),
+                    message: Text("탈퇴 처리가 성공적으로 완료되었습니다."),
+                    primaryButton: .default(Text("닫기"), action: {
+                        showDeleteCompleteAlert = false
+                        self.authState = .logout
+                    }), secondaryButton: .default(Text("확인").bold(), action: {
+                        showDeleteCompleteAlert = false
+                        self.authState = .logout
+                    })
+                )
+            }
             
-            Spacer()
-        }
-        .navigationTitle("설정")
-        .navigationBarTitleDisplayMode(.inline)
-        .background(Color.gray900)
-        .alert(isPresented: $showDeleteCompleteAlert) {
-            Alert(
-                title: Text("탈퇴 완료"),
-                message: Text("탈퇴 처리가 성공적으로 완료되었습니다."),
-                primaryButton: .default(Text("닫기"), action: {
-                    showDeleteCompleteAlert = false
-                    self.authState = .logout
-                }), secondaryButton: .default(Text("확인").bold(), action: {
-                    showDeleteCompleteAlert = false
-                    self.authState = .logout
-                })
-            )
+            switch authState {
+            case .lookAround:
+                ZStack {
+                    Color.customBlack.opacity(0.7)
+                    VStack {
+                        Spacer()
+                        LookAroundModalView {
+                            dismiss()
+                        }
+                    }
+                    .frame(height: UIScreen.main.bounds.height / 2)
+                    .frame(maxWidth: .infinity)
+                    .background {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .stroke(Color.customPrimary, lineWidth: 2)
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .foregroundStyle(Color.gray900)
+                    }
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                    .offset(y: 40)
+                }
+            default:
+                EmptyView()
+            }
         }
         .onAppear {
-            profileViewModel.loadUserInfo()
+            switch authState {
+            case .login:
+                profileViewModel.loadUserInfo()
+            default:
+                return
+            }
         }
     }
 }
