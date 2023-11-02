d2e88895c1aa2ebdfd550ad6e2b1e45e761ee5fb
Moonkey <moonkey48@icloud.com>
2023-11-02 21:00:03 +0900

    feat #205 : userInfo 수정 기능 구현

--- Outline/Outline/Source/View/Profile/ProfileView.swift
+++ Outline/Outline/Source/View/Profile/ProfileView.swift
@@ -18,112 +18,113 @@ struct ProfileView: View {
     @State private var userProfileImage = "defaultProfileImage"
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                Image(userProfileImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 118, height: 118)
-                    .padding(.top, 18)
-                
-                Text(profileViewModel.userInfo.nickname)
-                Divider()
-                    .frame(height: 1)
-                    .background(Color.gray700)
-                    .padding(.top, 20)
-                List {
-                    Group {
-                        NavigationLink {
-                            ProfileUserInfoView(profileViewModel: profileViewModel)
-                        } label: {
-                            Text("내 정보")
-                                .padding(.vertical, 5)
+        VStack {
+            Image(userProfileImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 118, height: 118)
+                .padding(.top, 18)
+            
+            Text(profileViewModel.userInfo.nickname)
+            Divider()
+                .frame(height: 1)
+                .background(Color.gray700)
+                .padding(.top, 20)
+            List {
+                Group {
+                    NavigationLink {
+                        ProfileUserInfoView(nickname: $profileViewModel.userInfo.nickname, birthday: $profileViewModel.userInfo.birthday, gender: $profileViewModel.userInfo.gender) {
+                            profileViewModel.saveUserInfoOnDB()
                         }
-                        NavigationLink {
-                            ProfileHealthInfoView()
-                        } label: {
-                            Text("신체 정보")
-                                .padding(.vertical, 5)
-                        }
+                    } label: {
+                        Text("내 정보")
+                            .padding(.vertical, 5)
                     }
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
-                }
-                .environment(\.defaultMinListRowHeight, 50)
-                .listStyle(.plain)
-                .frame(height: 100)
-                .padding(.top, 10)
-                
-                Rectangle()
-                    .fill(Color.gray800)
-                    .frame(height: 8)
-                    .padding(.top, 20)
-                List {
-                    Group {
-                        Button {
-                            showDeleteUserAlert = true
-                        } label: {
-                            Text("계정 삭제")
-                                .foregroundStyle(Color.customRed)
-                        }
-                        .alert(isPresented: $showDeleteUserAlert) {
-                            Alert(
-                                title: Text("계정 삭제"),
-                                message: Text("이별인가요? 너무아쉬워요. 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
-                                primaryButton: .default(Text("취소"), action: {
-                                    showDeleteUserAlert = false
-                                }), secondaryButton: .default(Text("삭제").bold(), action: {
-                                    showDeleteUserAlert = false
-                                    // handle delete User
-                                    showDeleteCompleteAlert = true
-                                }))
-                        }
-
-                        Button {
-                            showLogoutAlert = true
-                        } label: {
-                            Text("로그아웃")
-                        }
-                        .alert(isPresented: $showLogoutAlert) {
-                            Alert(
-                                title: Text("로그아웃"),
-                                message: Text("정말 로그아웃 하시겠나요?"),
-                                primaryButton: .default(Text("닫기"), action: {
-                                    showLogoutAlert = false
-                                }), secondaryButton: .default(Text("로그아웃").bold(), action: {
-                                    showLogoutAlert = false
-                                    // handle logout
-                                }))
-                        }
-
+                    NavigationLink {
+                        ProfileHealthInfoView()
+                    } label: {
+                        Text("신체 정보")
+                            .padding(.vertical, 5)
                     }
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
                 }
-                .environment(\.defaultMinListRowHeight, 50)
-                .listStyle(.plain)
-                .frame(height: 150)
-                .padding(.top, 10)
-                
-                Spacer()
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
             }
-            .navigationTitle("설정")
-            .navigationBarTitleDisplayMode(.inline)
-            .background(Color.gray900)
-            .alert(isPresented: $showDeleteCompleteAlert) {
-                Alert(
-                    title: Text("탈퇴 완료"),
-                    message: Text("탈퇴 처리가 성공적으로 완료되었습니다."),
-                    primaryButton: .default(Text("닫기"), action: {
-                        showDeleteCompleteAlert = false
-                    }), secondaryButton: .default(Text("확인").bold(), action: {
-                        showDeleteCompleteAlert = false
-                        // handle navigate to Login View
-                    })
-                )
+            .environment(\.defaultMinListRowHeight, 50)
+            .listStyle(.plain)
+            .frame(height: 100)
+            .padding(.top, 10)
+            
+            Rectangle()
+                .fill(Color.gray800)
+                .frame(height: 8)
+                .padding(.top, 20)
+            List {
+                Group {
+                    Button {
+                        showDeleteUserAlert = true
+                    } label: {
+                        Text("계정 삭제")
+                            .foregroundStyle(Color.customRed)
+                    }
+                    .alert(isPresented: $showDeleteUserAlert) {
+                        Alert(
+                            title: Text("계정 삭제"),
+                            message: Text("이별인가요? 너무아쉬워요. 계정을 삭제하면 러닝 정보, 경로 기록 등\n 모든 활동 정보가 삭제 됩니다."),
+                            primaryButton: .default(Text("취소"), action: {
+                                showDeleteUserAlert = false
+                            }), secondaryButton: .default(Text("삭제").bold(), action: {
+                                showDeleteUserAlert = false
+                                // handle delete User
+                                showDeleteCompleteAlert = true
+                            }))
+                    }
+
+                    Button {
+                        showLogoutAlert = true
+                    } label: {
+                        Text("로그아웃")
+                    }
+                    .alert(isPresented: $showLogoutAlert) {
+                        Alert(
+                            title: Text("로그아웃"),
+                            message: Text("정말 로그아웃 하시겠나요?"),
+                            primaryButton: .default(Text("닫기"), action: {
+                                showLogoutAlert = false
+                            }), secondaryButton: .default(Text("로그아웃").bold(), action: {
+                                showLogoutAlert = false
+                                // handle logout
+                            }))
+                    }
+
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
             }
+            .environment(\.defaultMinListRowHeight, 50)
+            .listStyle(.plain)
+            .frame(height: 150)
+            .padding(.top, 10)
+            
+            Spacer()
         }
-        .tint(Color.customPrimary)
+        .navigationTitle("설정")
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color.gray900)
+        .alert(isPresented: $showDeleteCompleteAlert) {
+            Alert(
+                title: Text("탈퇴 완료"),
+                message: Text("탈퇴 처리가 성공적으로 완료되었습니다."),
+                primaryButton: .default(Text("닫기"), action: {
+                    showDeleteCompleteAlert = false
+                }), secondaryButton: .default(Text("확인").bold(), action: {
+                    showDeleteCompleteAlert = false
+                })
+            )
+        }
+        .onAppear {
+            profileViewModel.loadUserInfo()
+        }
     }
 }
 
