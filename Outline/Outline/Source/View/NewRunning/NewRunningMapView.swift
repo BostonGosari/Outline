b1ce940bd24165f210b156bbcae2f4caffadbdf3
Hyebin (Helia) <hyebin218@naver.com>
2023-11-15 00:35:08 +0900

    feat #247 :  runningData 연결

--- Outline/Outline/Source/View/NewRunning/NewRunningMapView.swift
+++ Outline/Outline/Source/View/NewRunning/NewRunningMapView.swift
@@ -9,40 +9,67 @@ import MapKit
 import SwiftUI
 
 struct NewRunningMapView: View {
-        @State private var position: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
-        @State private var userlocations: [CLLocationCoordinate2D] = []
-        @Namespace var mapScope
-        
-        var body: some View {
-            ZStack(alignment: .topTrailing) {
-                Map(position: $position, scope: mapScope) {
-                    UserAnnotation()
-                    
-                    MapPolyline(coordinates: userlocations)
-                        .stroke(.customPrimary, style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
-                }
-                .mapControlVisibility(.hidden)
-                .onChange(of: CLLocationManager().location) { _, userlocation in
-                    if let user = userlocation {
-                        userlocations.append(user.coordinate)
-                    }
+    @StateObject var runningStartManager = RunningStartManager.shared
+    @StateObject var runningDataManager = RunningDataManager.shared
+    
+    @State private var showBigGuide = false
+    
+    @State private var position: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
+    @State private var userlocations: [CLLocationCoordinate2D] = []
+    @Namespace var mapScope
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Map(position: $position, scope: mapScope) {
+                UserAnnotation()
+                
+                if let courseGuide = runningStartManager.startCourse {
+                    MapPolyline(coordinates: ConvertCoordinateManager.convertToCLLocationCoordinates(courseGuide.coursePaths))
+                        .stroke(.black.opacity(0.3), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
                 }
                 
-                VStack(alignment: .trailing) {
-                    Rectangle()
-                        .frame(width: 113, height: 168)
-                        .foregroundStyle(.white.opacity(0.4))
-                    MapUserLocationButton(scope: mapScope)
-                        .buttonBorderShape(.circle)
-                        .tint(.white)
-                        .controlSize(.large)
-                }
-                .padding(.top, 80)
-                .padding(.trailing, 13)
+                MapPolyline(coordinates: userlocations)
+                    .stroke(.customPrimary, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
             }
-            .mapScope(mapScope)
-            .tint(.customPrimary)
+            .mapControlVisibility(.hidden)
+            .onChange(of: CLLocationManager().location) { _, userlocation in
+                if let user = userlocation {
+                    userlocations.append(user.coordinate)
+                }
+            }
+            
+            VStack {
+                guideView
+                    .frame(width: 113, height: 168)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                MapUserLocationButton(scope: mapScope)
+                    .buttonBorderShape(.circle)
+                    .tint(.white)
+                    .controlSize(.large)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                    .padding(.bottom, 96)
+                    .padding(.leading, 16)
+            }
+            .padding(.top, 80)
+            .padding(.trailing, 13)
         }
+        .mapScope(mapScope)
+        .tint(.customPrimary)
+    }
+    
+    private var guideView: some View {
+        ZStack {
+            if let course = runningStartManager.startCourse,
+               runningStartManager.runningType == .gpsArt {
+                CourseGuideView(
+                    userLocations: $userlocations,
+                    showBigGuide: $showBigGuide,
+                    coursePathCoordinates: ConvertCoordinateManager.convertToCLLocationCoordinates(course.coursePaths),
+                    courseRotate: course.heading
+                )
+            }
+        }
+    }
 }
 
 #Preview {
