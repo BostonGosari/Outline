a4daad497178fc1fb92da76271219651babaa458
Hyebin (Helia) <hyebin218@naver.com>
2023-11-15 17:05:44 +0900

    feat #253 : customNavigation 구현

--- Outline/Outline/Source/Manager/LocationManager.swift
+++ Outline/Outline/Source/Manager/LocationManager.swift
@@ -11,8 +11,12 @@ import SwiftUI
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var userLocations: [CLLocationCoordinate2D]
     @Published var isRunning: Bool
+    @Published var currentDirection: Route?
+    @Published var distance = 0.0
     
     private var locationManager = CLLocationManager()
+    private var navigationDatas = [Route]()
+    private var index = 1
     
     static let shared = LocationManager()
     
@@ -22,26 +26,68 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         
         super.init()
         
+        getNavigaionData()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = 10
         locationManager.startUpdatingLocation()
+        
+        setRegion()
     }
     
     private func getNavigaionData() {
+        let decoder = JSONDecoder()
+        guard let fileLocation = Bundle.main.url(forResource: "test", withExtension: "json") else { return }
         
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let jsonDictionary = try decoder.decode([String: [Route]].self, from: data)
+            
+            if let routes = jsonDictionary["Route"] {
+                navigationDatas = routes
+            } else {
+                print("JSON parsing error: Key 'Route' not found.")
+            }
+        } catch {
+            print("JSON 파싱 에러: \(error.localizedDescription)")
+        }
+        
+        currentDirection = navigationDatas[index]
+        print(currentDirection)
+    }
+    
+    private func setRegion() {
+        if let currentDirection = currentDirection {
+            let region = CLCircularRegion(
+                center: CLLocationCoordinate2D(latitude: currentDirection.latitude, longitude: currentDirection.longitude),
+                radius: 2,
+                identifier: "\(index)"
+            )
+            locationManager.startMonitoring(for: region)
+        }
+    }
+    
+    private func checkDistance(_ location: CLLocationCoordinate2D) {
+        if let direction = currentDirection {
+            let targetLocation =  CLLocation(latitude: direction.latitude, longitude: direction.longitude)
+            let currentLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
+            distance = targetLocation.distance(from: currentLocation)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if isRunning,
             let currentLocation = locations.last?.coordinate {
+            checkDistance(currentLocation)
             userLocations.append(currentLocation)
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        print("exit")
-        let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 36.01447, longitude: 129.328), radius: 2, identifier: "id1")
-        locationManager.startMonitoring(for: region)
+        if index+1 < navigationDatas.count {
+            index += 1
+            currentDirection = navigationDatas[index]
+            setRegion()
+        }
     }
 }
---
+++ Outline/Outline/Source/Model/Route.swift
@@ -0,0 +1,15 @@
+//
+//  Route.swift
+//  Outline
+//
+//  Created by hyebin on 11/15/23.
+//
+
+import Foundation
+
+struct Route: Codable {
+    var nextDirection: String
+    var alertMessage: String
+    var longitude: Double
+    var latitude: Double
+}
