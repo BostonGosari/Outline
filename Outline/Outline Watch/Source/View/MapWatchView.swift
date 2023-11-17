f6fa22f62467dfaf19fe8b1bcad94372b859c051
hyunjun <hyunjuntyler@gmail.com>
2023-11-17 19:07:30 +0900

    chore #222 : 경로관련 오류 수정

--- Outline/Outline Watch/Source/View/MapWatchView.swift
+++ Outline/Outline Watch/Source/View/MapWatchView.swift
@@ -22,10 +22,11 @@ struct MapWatchView: View {
                 MapPolyline(coordinates: ConvertCoordinateManager.convertToCLLocationCoordinates(watchConnectivityManager.receivedCourse.coursePaths))
                     .stroke(.white.opacity(0.5), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
             }
-                if !watchRunningManager.startCourse.coursePaths.isEmpty {
-                    MapPolyline(coordinates: ConvertCoordinateManager.convertToCLLocationCoordinates(watchRunningManager.startCourse.coursePaths))
-                        .stroke(.white.opacity(0.5), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
-                }
+            
+            if !watchRunningManager.startCourse.coursePaths.isEmpty {
+                MapPolyline(coordinates: ConvertCoordinateManager.convertToCLLocationCoordinates(watchRunningManager.startCourse.coursePaths))
+                    .stroke(.white.opacity(0.5), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+            }
             
             MapPolyline(coordinates: locationManager.userLocations)
                 .stroke(.customPrimary, style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
