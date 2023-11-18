a74c5787356fa97979949278f5c1f388a23ea49d
chamroro <chamsae@handong.ac.kr>
2023-11-18 14:07:44 +0900

    design #261 : underline button 컴포넌트화

---
+++ Outline/Outline/Source/Utils/Component/UnderlineButton.swift
@@ -0,0 +1,35 @@
+//
+//  UnderlineButton.swift
+//  Outline
+//
+//  Created by 김하은 on 11/18/23.
+//
+
+import SwiftUI
+
+struct UnderlineButton: View {
+    let text: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Text(text)
+                    .foregroundStyle(Color.gray300)
+                    .font(.customSubbody)
+                    .background(
+                       Rectangle()
+                           .frame(height: 1)
+                           .foregroundColor(Color.gray300)
+                           .offset(y: 10)
+                   )
+            }
+        }
+    }
+}
+
+#Preview {
+    UnderlineButton(text: "밑줄 버튼") {
+        
+    }
+}
