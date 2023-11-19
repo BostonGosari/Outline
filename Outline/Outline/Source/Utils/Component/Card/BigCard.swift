cb340ae02ec5c4699ae6acd213985badddc57ab8
hyunjun <hyunjuntyler@gmail.com>
2023-11-19 12:20:28 +0900

    feat #262 : MotionManager 추가

--- Outline/Outline/Source/Utils/Component/Card/BigCard.swift
+++ Outline/Outline/Source/Utils/Component/Card/BigCard.swift
@@ -5,6 +5,7 @@
 //  Created by hyunjun on 11/18/23.
 //
 
+import CoreMotion
 import SwiftUI
 
 enum CardType {
@@ -42,34 +43,27 @@ enum CardType {
 }
 
 struct BigCard: View {
+    @StateObject var manager = MotionManager()
     @State var translation: CGSize = .zero
     @State var isDragging = false
     @State var snapShotAngle: Double = 0
     @State var rotationAngle: Double = 0
     @State var isFrontSide = true
     @State var isFliped = false
+    @State var isDragable = false
+    @State var cardFloatingOffset: CGFloat = 0
     
     var cardType: CardType = .good
     var runName: String = "돌고래런"
     var date: String = "2023.11.19"
+    var editMode: Bool = false
     
     var body: some View {
         ZStack {
             if isFrontSide {
                 BigCardFrontSide(cardType: cardType, runName: runName, date: date)
             } else {
-                BigCardBackSide(cardType: cardType, runName: runName, date: date)
-            }
-        }
-        .overlay {
-            VStack {
-                Text("\(snapShotAngle)")
-                Text("\(rotationAngle)")
-                Text("\(isFliped.description)")
-            }
-            .background {
-                Rectangle()
-                    .foregroundStyle(.black)
+                BigCardBackSide(cardType: cardType, runName: runName, date: date, editMode: editMode)
             }
         }
         .rotation3DEffect(
@@ -81,9 +75,10 @@ struct BigCard: View {
             ),
             perspective: 0.3
         )
-        .gesture(drag)
+        .offset(y: cardFloatingOffset)
+        .gesture(isDragable ? drag : nil)
         .onAppear {
-            onAppearRotateCard()
+            onAppearCardAnimation()
         }
         .onChange(of: snapShotAngle) { oldValue, newValue in
             if abs(Int(newValue - oldValue)) / 180 % 2 > 0 {
@@ -95,9 +90,9 @@ struct BigCard: View {
     private var drag: some Gesture {
         DragGesture()
             .onChanged { value in
-                withAnimation(.bouncy) {
-                    rotationAngle = value.translation.width * 2
-                    DispatchQueue.main.async {
+                DispatchQueue.main.async {
+                    withAnimation(.bouncy) {
+                        rotationAngle = value.translation.width * 2
                         updateCardSide()
                     }
                 }
@@ -121,16 +116,29 @@ struct BigCard: View {
             )
     }
     
-    private func onAppearRotateCard() {
-        withAnimation(.bouncy(duration: 6)) {
+    private func onAppearCardAnimation() {
+        withAnimation(.bouncy(duration: 4)) {
             rotationAngle += 360
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
             isFrontSide.toggle()
         }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
             isFrontSide.toggle()
             rotationAngle = 0
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            withAnimation(.easeIn(duration: 0.5)) {
+                cardFloatingOffset = -5
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+            withAnimation(.bouncy(duration: 1.5, extraBounce: 0.65)) {
+                cardFloatingOffset = 0
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            isDragable = true
         }
     }
     
@@ -138,6 +146,30 @@ struct BigCard: View {
         let absRotationAngle = abs(rotationAngle)
         let isOddRotation = Int((absRotationAngle + 90) / 180) % 2 != 0
         isFrontSide = (isOddRotation && isFliped) || (!isOddRotation && !isFliped)
+    }
+}
+
+final class MotionManager: ObservableObject {
+    @Published var pitch: Double = 0.0
+    @Published var roll: Double = 0.0
+    
+    private var manager: CMMotionManager
+    
+    init() {
+        self.manager = CMMotionManager()
+        self.manager.deviceMotionUpdateInterval = 1/60
+        self.manager.startDeviceMotionUpdates(to: .main) { (motionData, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            if let motionData = motionData {
+                self.pitch = motionData.attitude.pitch
+                self.roll = motionData.attitude.roll
+            }
+        }
+        
     }
 }
 
