03a61657bc8e8bb175a050c4dafd9bb0b9c09a73
Moonkey <moonkey48@icloud.com>
2023-11-20 11:40:18 +0900

    feat #267 : CourseScoreModelType 생성

---
+++ Outline/Outline/Source/Model/Score/CourseScoreModel.swift
@@ -0,0 +1,8 @@
+//
+//  CourseScoreModel.swift
+//  Outline
+//
+//  Created by Seungui Moon on 11/20/23.
+//
+
+
---
+++ Outline/Outline/Source/Model/Score/CourseScoreModelType.swift
@@ -0,0 +1,13 @@
+//
+//  ScoreModelType.swift
+//  Outline
+//
+//  Created by Seungui Moon on 11/20/23.
+//
+
+import SwiftUI
+
+struct CourseScoreSet {
+    var id: String
+    var score: Int
+}
