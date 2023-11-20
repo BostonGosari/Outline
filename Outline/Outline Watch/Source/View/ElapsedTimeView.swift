a9b4488905e425b56afdac9fbf28c4700ce69cc2
hyunjun <hyunjuntyler@gmail.com>
2023-11-20 17:10:03 +0900

    design #270 : 폰트 시스템 적용

--- Outline/Outline Watch/Source/View/ElapsedTimeView.swift
+++ Outline/Outline Watch/Source/View/ElapsedTimeView.swift
@@ -13,9 +13,9 @@ struct ElapsedTimeView: View {
 
     var body: some View {
         Text(NSNumber(value: elapsedTime), formatter: timeFormatter)
+            .font(Font.customHeadline)
             .monospacedDigit()
             .foregroundStyle(.customPrimary)
-            .font(.system(size: 40, weight: .bold))
             .padding(.bottom)
     }
 }
