5f1272d4bdcbcc07b7d9c1f72c25a72c29351883
Hyebin (Helia) <hyebin218@naver.com>
2023-11-21 19:10:54 +0900

    feat #276 : 새로운 공유뷰 구현

--- Outline/Outline/Source/View/Share/ShareView.swift
+++ Outline/Outline/Source/View/Share/ShareView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ShareView: View {
     @Environment(\.dismiss) var dismiss
+    @StateObject private var viewModel = ShareViewModel()
 
+    @State private var size: CGSize = .zero
     @State private var scale: CGFloat = 1
     @State private var lastScale: CGFloat = 0
     @State private var offset: CGSize = .zero
@@ -21,89 +23,184 @@ struct ShareView: View {
     @State private var pathWidth: CGFloat = 0
     @State private var pathHeight: CGFloat = 0
     
+    @State private var image: UIImage?
+    
     let runningData: ShareModel
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color.gray900
-                    .ignoresSafeArea()
-                
-                VStack(spacing: 0) {
-                    ZStack {
-                        ZStack {
-                            Image("ShareFirstLayer")
-                            Image("ShareSecondLayer")
-                            Image("ShareThirdLayer")
-                        }
-                            .padding(.top, 16)
-                            .padding(.bottom, 46)
-                        
-                        VStack(alignment: .trailing) {
-                            Text("Time 00:20")
-                            Text("Pace 9'99\"")
-                            Text("Distance 1.2km")
-                            Text("Kcal 235")
-                        }
-                        .font(.customSubbody)
-                        .lineSpacing(16)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                        .padding(.trailing, 50)
-                        .padding(.bottom, 46)
-                        
-                        ZStack {
-                            Color.black.opacity(0.001)
-                            
-                            PathGenerateManager
-                                .caculateLines(width: 200, height: 200, coordinates: runningData.userLocations)
-                                .stroke(.customPrimary, style: .init(lineWidth: 5, lineCap: .round, lineJoin: .round))
-                        }
-                        .frame(width: pathWidth + 30, height: pathHeight + 30)
-                        .scaleEffect(scale)
-                        .offset(offset)
-                        .rotationEffect(lastAngle + angle)
-                        .gesture(dragGesture)
-                        .gesture(rotationGesture)
-                        .simultaneousGesture(magnificationGesture)
-                    }
-                    
-                    HStack(spacing: 0) {
-                        Button {
-                        } label: {
-                            Image(systemName: "square.and.arrow.down")
-                                .foregroundStyle(Color.black)
-                                .font(.system(size: 20, weight: .semibold))
-                                .padding(.vertical, 14)
-                                .padding(.horizontal, 19)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill(Color.customWhite)
-                                }
-                        }
-                        .padding(.leading, 16)
-                        
-                        CompleteButton(text: "Instagram으로 공유하기", isActive: true) {
-                        }
-                        .padding(.leading, -8)
-                    }
-                    .frame(maxHeight: .infinity, alignment: .bottom)
+        ZStack {
+            Color.gray900
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                ZStack {
+                    shareImage
+                    runningInfo
+                    userPath
+                }
+                buttons
+            }
+        }
+        .navigationTitle("공유")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    (Text(Image(systemName: "chevron.left")) + Text("뒤로"))
+                        .foregroundStyle(Color.customPrimary)
                 }
             }
-            .navigationTitle("공유")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        (Text(Image(systemName: "chevron.left")) + Text("뒤로"))
-                            .foregroundStyle(Color.customPrimary)
-                    }
-                }
+        }
+        .sheet(isPresented: $viewModel.permissionDenied) {
+            PermissionSheet(permissionType: .photoLibrary)
+        }
+        .sheet(isPresented: $viewModel.isShowInstaAlert) {
+            instaSheet
+        }
+        .overlay {
+            if viewModel.isShowPopup {
+                RunningPopup(text: "이미지 저장이 완료되었어요.")
+                    .frame(maxHeight: .infinity, alignment: .top)
             }
         }
     }
 }
+
+extension ShareView {
+    private var shareImage: some View {
+        ZStack {
+            backgroundImage
+            runningInfo
+            
+            GeometryReader { proxy in
+                HStack {}
+                    .onAppear {
+                        self.size = CGSize(width: proxy.size.width, height: proxy.size.height)
+                    }
+            }
+        }
+    }
+    
+    private var backgroundImage: some View {
+        ZStack {
+            Image("ShareFirstLayer")
+            Image("ShareSecondLayer")
+                .background(
+                    TransparentBlurView(removeAllFilters: true)
+                        .blur(radius: 3, opaque: true)
+                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            Image("ShareThirdLayer")
+        }
+        .padding(.top, 16)
+        .padding(.bottom, 46)
+    }
+    
+    private var runningInfo: some View {
+        VStack(alignment: .trailing, spacing: -3) {
+            Text("Time \(runningData.time)")
+            Text("Pace \(runningData.pace)")
+            Text("Distance \(runningData.distance)")
+            Text("Kcal \(runningData.cal)")
+        }
+        .font(.customSubbody)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+        .padding(.trailing, 50)
+        .padding(.bottom, 57)
+    }
+    
+    private var userPath: some View {
+        ZStack {
+            Color.black.opacity(0.001)
+            PathGenerateManager
+                .caculateLines(width: 200, height: 200, coordinates: runningData.userLocations)
+                .stroke(.customPrimary, style: .init(lineWidth: 5, lineCap: .round, lineJoin: .round))
+        }
+        .frame(width: pathWidth + 30, height: pathHeight + 30)
+        .scaleEffect(scale)
+        .offset(offset)
+        .rotationEffect(lastAngle + angle)
+        .gesture(dragGesture)
+        .gesture(rotationGesture)
+        .simultaneousGesture(magnificationGesture)
+    }
+    
+    private var buttons: some View {
+        HStack(spacing: 0) {
+            Button {
+                if let img = image {
+                    viewModel.saveImage(image: img)
+                }
+            } label: {
+                Image(systemName: "square.and.arrow.down")
+                    .foregroundStyle(Color.black)
+                    .font(.system(size: 20, weight: .semibold))
+                    .padding(.vertical, 14)
+                    .padding(.horizontal, 19)
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color.customWhite)
+                    }
+            }
+            .padding(.leading, 16)
+            
+            CompleteButton(text: "Instagram으로 공유하기", isActive: true) {
+                if let img = image {
+                    viewModel.shareToInstagram(image: img)
+                }
+                
+            }
+            .padding(.leading, -8)
+        }
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .padding(.bottom, 8)
+    }
+    
+    private var instaSheet: some View {
+        ZStack {
+            Color.gray900.ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 35, style: .continuous)
+                .stroke(lineWidth: 2)
+                .foregroundStyle(Gradient(colors: [.customPrimary, .gray900, .gray900, .gray900]))
+                .ignoresSafeArea()
+            
+            VStack(spacing: 10) {
+                Text("Instagram 설치")
+                    .font(.customTitle2)
+                    .fontWeight(.semibold)
+                    .padding(.top, 36)
+                Text("Instagram을 설치해야 아트를 공유할 수 있어요.")
+                    .font(.customSubbody)
+                    .foregroundColor(.gray300)
+                    .multilineTextAlignment(.center)
+                Image("InstaImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120)
+                    .padding(.bottom, 32)
+                Button {
+                    viewModel.isShowInstaAlert = false
+                } label: {
+                    Text("확인")
+                        .font(.customButton)
+                        .foregroundStyle(Color.customBlack)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 15)
+                                .foregroundStyle(Color.customPrimary)
+                        }
+                }
+                .padding()
+            }
+            .presentationDetents([.height(400)])
+            .presentationCornerRadius(35)
+        }
+    }
+}
+
 extension ShareView {
     private var dragGesture: some Gesture {
         return DragGesture()
@@ -157,5 +254,5 @@ extension ShareView {
 }
 
 #Preview {
-    ShareView(runningData: ShareModel(courseName: "고래런", runningDate: "2023.11.21", regionDisplayName: "포항시 지곡동", distance: "32.2km", cal: "235", pace: "9'99\"", bpm: "--", time: "00:20", userLocations: []))
+    ShareView(runningData: ShareModel(distance: "32.2km", cal: "235", pace: "9'99\"", time: "00:20", userLocations: []))
 }
