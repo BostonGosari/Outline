1fab40d497aa4a7b5bb326ca4df3dfa164cd7343
Hyebin (Helia) <hyebin218@naver.com>
2023-11-21 19:34:07 +0900

    fix #276 : ShareModel 수정

--- Outline/Outline/Source/Model/ShareModel.swift
+++ Outline/Outline/Source/Model/ShareModel.swift
@@ -8,24 +8,36 @@
 import CoreLocation
 
 struct ShareModel {
+    var courseName: String
+    var runningDate: String
+    var regionDisplayName: String
     var distance: String
     var cal: String
     var pace: String
+    var bpm: String
     var time: String
     var userLocations: [CLLocationCoordinate2D]
     
     init() {
+        self.courseName = ""
+        self.runningDate = ""
+        self.regionDisplayName = ""
         self.distance = ""
         self.cal = ""
         self.pace = ""
+        self.bpm = ""
         self.time = ""
         self.userLocations = []
     }
     
-    init(distance: String, cal: String, pace: String, time: String, userLocations: [CLLocationCoordinate2D]) {
+    init(courseName: String, runningDate: String, regionDisplayName: String, distance: String, cal: String, pace: String, bpm: String, time: String, userLocations: [CLLocationCoordinate2D]) {
+        self.courseName = courseName
+        self.runningDate = runningDate
+        self.regionDisplayName = regionDisplayName
         self.distance = distance
         self.cal = cal
         self.pace = pace
+        self.bpm = bpm
         self.time = time
         self.userLocations = userLocations
     }
