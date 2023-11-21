341145e2f3e3f0e915a84ebf1a5bd44788e918fb
Moonkey <moonkey48@icloud.com>
2023-11-21 16:15:12 +0900

    feat #288 : AppStorage로 voice 권한 설정 기능 추가 구현

--- Outline/Outline/Source/View/Profile/ProfileVoiceView.swift
+++ Outline/Outline/Source/View/Profile/ProfileVoiceView.swift
@@ -8,32 +8,36 @@
 import SwiftUI
 
 struct ProfileVoiceView: View {
-    @State private var isVoiceOniPhone = true
-    @State private var isVoiceOnWatch = true
+    @Binding var isVoiceOniPhone: Bool
+    @Binding var isVoiceOnWatch: Bool
     
     var body: some View {
-        List {
-            Toggle(isOn: $isVoiceOniPhone) {
-                Text("iPhone 음성 피드백")
-                    .font(.customSubbody)
+        VStack {
+            Spacer()
+                .frame(height: 26)
+            List {
+                Toggle(isOn: $isVoiceOniPhone) {
+                    Text("iPhone 음성 피드백")
+                        .font(.customSubbody)
+                }
+                .listRowBackground(Color.customBlack)
+                .toggleStyle(SwitchToggleStyle(tint: .customGreen))
+                .padding(.vertical, 10)
+                
+                Toggle(isOn: $isVoiceOnWatch) {
+                    Text("Apple Watch 음성 피드백")
+                        .font(.customSubbody)
+                }
+                .listRowBackground(Color.customBlack)
+                .padding(.vertical, 4)
+                .toggleStyle(SwitchToggleStyle(tint: .customGreen))
+                .padding(.vertical, 10)
+                .listRowSeparator(.hidden)
             }
-            .listRowBackground(Color.customBlack)
-            .padding(.vertical, 30)
-            Toggle(isOn: $isVoiceOnWatch) {
-                Text("Apple Watch 음성 피드백")
-                    .font(.customSubbody)
-            }
-            .listRowBackground(Color.customBlack)
-            .padding(.vertical, 4)
+            .background(.gray900)
+            .navigationTitle("음성 설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .listStyle(.grouped)
         }
-        .background(.customBlack)
-        .navigationTitle("음성 설정")
-        .navigationBarTitleDisplayMode(.inline)
-        .listStyle(PlainListStyle())
-
     }
-}
-
-#Preview {
-    ProfileVoiceView()
 }
