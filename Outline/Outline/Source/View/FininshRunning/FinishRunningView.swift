ade5d2cdd23d43f1a41d886dbfd256d5c680551f
hyunjun <hyunjuntyler@gmail.com>
2023-11-27 22:06:54 +0900

    design #317 : 디테일뷰 배경 원형 블러

--- Outline/Outline/Source/View/FininshRunning/FinishRunningView.swift
+++ Outline/Outline/Source/View/FininshRunning/FinishRunningView.swift
@@ -26,6 +26,19 @@ struct FinishRunningView: View {
         ZStack {
             Color.gray900
                 .ignoresSafeArea()
+            Circle()
+                .frame(width: 350)
+                .foregroundStyle(.customPrimary.opacity(0.35))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(x: -175, y: -100)
+                .blur(radius: 120)
+            Circle()
+                .frame(width: 350)
+                .foregroundStyle(.customPrimary.opacity(0.35))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                .offset(x: 100, y: 100)
+                .blur(radius: 120)
+            
             VStack {
                 BigCard(
                     cardType: .great,
