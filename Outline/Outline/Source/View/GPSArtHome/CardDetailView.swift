845407bb5d24fa37cefcb6e04dcc92537c9c0d3d
hyunjun <hyunjuntyler@gmail.com>
2023-11-29 17:44:46 +0900

    fix #323: 여러줄일때 텍스트 안보이는 문제 수정

--- Outline/Outline/Source/View/GPSArtHome/CardDetailView.swift
+++ Outline/Outline/Source/View/GPSArtHome/CardDetailView.swift
@@ -61,6 +61,7 @@ struct CardDetailView: View {
                                     .foregroundStyle(.clear)
                             }
                         }
+                        
                         CardDetailInformationView(
                             showCopyLocationPopup: $showCopyLocationPopup, selectedCourse: selectedCourse.course
                         )
