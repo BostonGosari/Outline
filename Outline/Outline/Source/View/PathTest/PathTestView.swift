0b9d924c4d615a733404959f36386b40f4e34388
Hyunjun <hyunjuntyler@gmail.com>
2024-01-23 03:36:54 +0900

    feat #340 : 보정값 적용

--- Outline/Outline/Source/View/PathTest/PathTestView.swift
+++ Outline/Outline/Source/View/PathTest/PathTestView.swift
@@ -13,19 +13,22 @@ struct PathTestView: View {
     @State private var coordinates: [Coordinate] = []
     
     private let parseManager = KMLParserManager()
-    private let width = 200.0
-    private let height = 200.0
-    private let fileName = "fish"
+    private var width = 300.0
+    private var height = 300.0
+    private let fileName = "seoulOctopusRun"
     
     var body: some View {
         ZStack {
                 PathManager.createPath(width: width, height: height, coordinates: coordinates.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }))
                     .stroke(.green, style: .init(lineWidth: 4, lineCap: .round, lineJoin: .round) )
                     .frame(width: PathManager.getCanvasData(coordinates: coordinates.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }), width: width, height: height).width, height: PathManager.getCanvasData(coordinates: coordinates.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }), width: width, height: height).height)
+                    .background {
+                        Color.white.opacity(0.2)
+                    }
                     .overlay {
                         VStack {
-                            Text("\(PathManager.getCanvasData(coordinates: coordinates.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }), width: width, height: height).width)")
-                            Text("\(PathManager.getCanvasData(coordinates: coordinates.map({ CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }), width: width, height: height).height)")
+                            Text("\(PathManager.getCanvasData(coordinates: coordinates.toCLLocationCoordinates(), width: width, height: height).width)")
+                            Text("\(PathManager.getCanvasData(coordinates: coordinates.toCLLocationCoordinates(), width: width, height: height).height)")
                         }
                     }
         }
