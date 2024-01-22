6fc7e36eb1b51cc6e1f3edccb6c79c40f1c5bfdb
Hyunjun <hyunjuntyler@gmail.com>
2024-01-23 00:54:47 +0900

    feat #340 : CoordinateExtension 생성

---
+++ Outline/Outline/Source/Utils/Extension/CoordinateExtension.swift
@@ -0,0 +1,30 @@
+//
+//  CoordinateExtension.swift
+//  Outline
+//
+//  Created by Hyunjun Kim on 1/23/24.
+//
+
+import CoreLocation
+
+extension Coordinate {
+    // Coordinate to CLLocationCoordinate2D
+    func toCLLocationCoordinate2D() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    init(from clLocationCoordinate: CLLocationCoordinate2D) {
+        self.init(longitude: clLocationCoordinate.longitude, latitude: clLocationCoordinate.latitude)
+    }
+}
+
+extension CLLocationCoordinate2D {
+    // CLLocationCoordinate2D to Coordinate
+    func toCoordinate() -> Coordinate {
+        return Coordinate(longitude: longitude, latitude: latitude)
+    }
+
+    init(from coordinate: Coordinate) {
+        self.init(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+}
