add4c0c9b5e1ee6aa23ddb6f4962f037eeced403
Hyebin (Helia) <hyebin218@naver.com>
2024-01-23 18:40:19 +0900

    feat #338 : MirroringMapView 수정

--- Outline/Outline/Source/View/Mirroring/MirroringMapView.swift
+++ Outline/Outline/Source/View/Mirroring/MirroringMapView.swift
@@ -8,37 +8,69 @@
 import MapKit
 import SwiftUI
 
-struct MirroringMapView: View {
+struct MirroringMapView: UIViewRepresentable {
     @StateObject private var connectivityManager = WatchConnectivityManager.shared
-    @State private var position: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
-    @Namespace private var mapScope
     
-    var body: some View {
-        ZStack(alignment: .topTrailing) {
-            Map(position: $position, scope: mapScope) {
-                let userLocations = ConvertCoordinateManager.convertToCLLocationCoordinates(connectivityManager.runningData.userLocations)
-                let course = ConvertCoordinateManager.convertToCLLocationCoordinates(connectivityManager.runningInfo.course)
-                UserAnnotation()
-                
-                MapPolyline(coordinates: course)
-                    .stroke(.black.opacity(0.3), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
-                
-                MapPolyline(coordinates: userLocations)
-                    .stroke(.customPrimary, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
-            }
-            .mapStyle(.standard(pointsOfInterest: []))
-            .mapControlVisibility(.hidden)
-            
-            MapUserLocationButton(scope: mapScope)
-                .buttonBorderShape(.circle)
-                .tint(.white)
-                .controlSize(.large)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
-                .padding(.bottom, 96)
-                .padding(.leading, 16)
+    private let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> MKMapView {
+        mapView.delegate = context.coordinator
+        mapView.showsUserLocation = true
+        
+        // tracking Button custom
+        let trackingButton = MKUserTrackingButton(mapView: mapView)
+        mapView.addSubview(trackingButton)
+        
+        trackingButton.translatesAutoresizingMaskIntoConstraints = false
+        trackingButton.trailingAnchor.constraint(equalTo: mapView.layoutMarginsGuide.trailingAnchor, constant: -16).isActive = true
+        trackingButton.topAnchor.constraint(equalTo: mapView.layoutMarginsGuide.topAnchor, constant: 42).isActive = true
+        trackingButton.backgroundColor = .black
+        
+        let course = ConvertCoordinateManager.convertToCLLocationCoordinates(connectivityManager.runningInfo.course)
+        
+        let polyline = MKPolyline(coordinates: course, count: course.count)
+        mapView.addOverlay(polyline)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let userLocations = ConvertCoordinateManager.convertToCLLocationCoordinates(connectivityManager.runningData.userLocations)
+        
+        if uiView.overlays.count >= 2,
+           let overlay = uiView.overlays.last {
+            uiView.removeOverlay(overlay)
         }
-        .mapScope(mapScope)
-        .tint(.customPrimary)
+        
+        if !userLocations.isEmpty {
+            let polyline = MKPolyline(
+                coordinates: userLocations,
+                count: userLocations.count
+            )
+            uiView.addOverlay(polyline)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MirroringMapView
+        
+        init(_ parent: MirroringMapView) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let polyline = overlay as? MKPolyline {
+                let renderer = MKPolylineRenderer(polyline: polyline)
+                renderer.strokeColor = (mapView.overlays.count == 1) ? UIColor(Color.black.opacity(0.3)) : .customPrimary
+                renderer.lineWidth = 5
+                return renderer
+            }
+            return MKOverlayRenderer(overlay: overlay)
+        }
     }
 }
 
