7587cd3a5ac1c600bf06063ac504383a5d464c56
Moonkey <moonkey48@icloud.com>
2024-03-04 22:54:23 +0900

    refactor #377 : ShareViewModel 기능 구체화

--- Outline/Outline/Source/ViewModel/ShareViewModel.swift
+++ Outline/Outline/Source/ViewModel/ShareViewModel.swift
@@ -11,6 +11,14 @@ import SwiftUI
 class ShareViewModel: ObservableObject {
     @Published var permissionDenied = false
     @Published var isShowInstaAlert = false
+    @Published var size: CGSize = .zero
+    @Published var scale: CGFloat = 1
+    @Published var lastScale: CGFloat = 0
+    @Published var offset: CGSize = .zero
+    @Published var lastStoredOffset: CGSize = .zero
+    @Published var angle: Angle = .degrees(0)
+    @Published var lastAngle: Angle = .degrees(0)
+    @Published var renderedImage: UIImage?
     
     @Published var isShowPopup = false {
         didSet {
@@ -59,6 +67,37 @@ class ShareViewModel: ObservableObject {
         if let url = URL(string: UIApplication.openSettingsURLString) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
+    }
+    
+    @MainActor
+    func onTapSaveImageButton(shareImageCombined: some View, isSave: Bool = true) {
+        renderShareView(shareImageCombined, isSave)
+        if let img = renderedImage {
+            DispatchQueue.main.async {
+                self.saveImage(image: img)
+            }
+        }
+    }
+    
+    @MainActor
+    func onTapUploadImageButton(shareImageCombined: some View, isSave: Bool = true) {
+        // TODO: 여기서부터 추후 sheet 올라오는 기능, 권한 체크, 사진 올리는 기능 구현
+        renderShareView(shareImageCombined, isSave)
+        if let img = renderedImage {
+            shareToInstagram(image: img)
+        }
+    }
+    
+    @MainActor 
+    func renderShareView(_ shareImageCombined: some View, _ isSave: Bool = true) {
+        renderedImage = shareImageCombined
+            .background(isSave ? .gray900 : .clear)
+            .render(scale: 3)
+    }
+    
+    @MainActor
+    func onAppearSharedImageCombined(size: CGSize) {
+        self.size = CGSize(width: size.width - 30, height: size.height - 70)
     }
 }
 
