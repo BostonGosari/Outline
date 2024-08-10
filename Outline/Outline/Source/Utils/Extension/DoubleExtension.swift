839745674a5453835fd5d7ce1b0427b8cb9d15eb
hyunjun <hyunjuntyler@gmail.com>
2024-08-10 17:46:32 +0900

    refactor #400 : Extension 분리 및 정리

--- Outline/Outline/Source/Utils/Extension/DoubleExtension.swift
+++ Outline/Outline/Source/Utils/Extension/DoubleExtension.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 extension Double {
-    
     func formattedCurrentPace() -> String {
         if self.isZero {
             return "-'--''"
@@ -27,12 +26,6 @@ extension Double {
         let minutes = Int(self / 60)
         let seconds = Int(self) % 60
         return String(format: "%d'%02d''", minutes, seconds)
-    }
-    
-    func formatDuration() -> String {
-        let hours = Int(self) / 60
-        let minutes = Int(self) % 60
-        return String(format: "%dh%02dm", hours, minutes)
     }
     
     func formatDurationInKoreanDetail() -> String {
@@ -58,21 +51,5 @@ extension Double {
         let minutes = Int(self) / 60
         let seconds = Int(self) % 60
         return String(format: "%d:%02d", minutes, seconds)
-    }
-    
-    func formatMinuteSecondsMilliSeconds() -> String {
-        let totalSeconds = Int(self)
-        let minutes = totalSeconds / 60
-        let seconds = totalSeconds % 60
-        let milliseconds = Int((self - Double(totalSeconds)) * 100)
-        
-        return String(format: "%d:%02d.%02d", minutes, seconds, milliseconds)
-    }
-    
-    func formatHourMinuteSeconds() -> String {
-        let hour = (Int(self) / 60) / 60
-        let minutes = (Int(self) / 60) % 60
-        let seconds = Int(self) % 60
-        return String(format: "%2d:%2d:%02d", hour, minutes, seconds)
     }
 }
