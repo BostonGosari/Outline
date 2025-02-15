870ec10ed7e8f58b81e726dc450ce48a4ff6421b
moonkey48 <moonkey48@icloud.com>
2025-02-15 19:31:00 +0900

    feature: 가독성 개선을 위해 CourseModel 비지니스 로직 async-await 형태 오버로딩

--- Outline/Outline/Source/Model/Course/CourseModel.swift
+++ Outline/Outline/Source/Model/Course/CourseModel.swift
@@ -23,7 +23,7 @@ enum GPSArtError: Error {
 struct CourseModel: CourseModelProtocol {
     private let courseListRef = Firestore.firestore().collection("allGPSArtCourses")
     private let courseCategoryRef = Firestore.firestore().collection("artCategories")
-    
+
     func readAllCourses(completion: @escaping (Result<[GPSArtCourse], GPSArtError>) -> Void) {
         courseListRef.getDocuments { (snapshot, error) in
             guard let snapshot = snapshot, error == nil else {
@@ -99,6 +99,78 @@ struct CourseModel: CourseModelProtocol {
                 completion(.success(courseCategory))
             } catch {
                 completion(.failure(.typeError))
+            }
+        }
+    }
+}
+
+/// async-await 
+extension CourseModel {
+    func readAllCourses() async throws -> [GPSArtCourse] {
+        return try await withCheckedThrowingContinuation { coninuation in
+            courseListRef.getDocuments { (snapshot, error) in
+                guard let snapshot = snapshot, error == nil else {
+                    coninuation.resume(throwing: CoreDataError.dataNotFound)
+                    return
+                }
+                do {
+                    var courseList: [GPSArtCourse] = []
+                    for snapshot in snapshot.documents {
+                        let course = try snapshot.data(as: GPSArtCourse.self)
+                        courseList.append(course)
+                    }
+                    coninuation.resume(returning: courseList)
+                } catch {
+                    coninuation.resume(throwing: CoreDataError.dataNotFound)
+                }
+            }
+        }
+    }
+
+    func readAllCategories() async throws -> [CourseCategory] {
+        return try await withCheckedThrowingContinuation { continuation in
+            courseCategoryRef.getDocuments { (snapshot, error) in
+                guard let snapshot = snapshot, error == nil else {
+                    continuation.resume(throwing: GPSArtError.dataNotFound)
+                    return
+                }
+                do {
+                    var courseCategories: [CourseCategory] = []
+                    for snapshot in snapshot.documents {
+                        let courseCategory = try snapshot.data(as: CourseCategory.self)
+                        courseCategories.append(courseCategory)
+                    }
+                    continuation.resume(returning: courseCategories)
+                } catch {
+                    continuation.resume(throwing: GPSArtError.dataNotFound)
+                }
+            }
+        }
+    }
+
+    func readCategoryCourse(categoryType: CourseCategoryType) async throws -> CourseCategory {
+        return try await withCheckedThrowingContinuation { continuation in
+            var categoryId: String = ""
+            switch categoryType {
+            case .category1:
+                categoryId = "GPSArtHome_Category1"
+            case .category2:
+                categoryId = "GPSArtHome_Category2"
+            case .category3:
+                categoryId = "GPSArtHome_Category3"
+            }
+            courseCategoryRef.document(categoryId).getDocument { (snapshot, error) in
+                guard let snapshot = snapshot, snapshot.exists, error == nil else {
+                    continuation.resume(throwing: GPSArtError.dataNotFound)
+                    return
+                }
+
+                do {
+                    let courseCategory = try snapshot.data(as: CourseCategory.self)
+                    continuation.resume(returning: courseCategory)
+                } catch {
+                    continuation.resume(throwing: GPSArtError.typeError)
+                }
             }
         }
     }
